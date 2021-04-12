777fdf06bdd6cf382b6b2a0f25a2ee7d4f33fea3
Madhav Pandurang Desai <madhav@ee.iitb.ac.in>
2021-04-12 21:25:59 +0530

    First implementation of FIFO constrained memory allocator.

---
+++ C/mempool/util/test_mempool.c
@@ -0,0 +1,144 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "mempool.h"
+
+#define NPAGES     8
+
+MemPool 	pool;
+MemPoolRequest 	req;
+MemPoolResponse resp;
+
+int _err_ = 0;
+
+int allocateAndFillPages(uint32_t* rtags, uint32_t* base_addresses)
+{
+	
+	uint32_t I;
+	for(I = 1; I <= NPAGES; I++)
+	{
+
+		req.request_type = ALLOCATE;
+		req.request_tag  = I;
+		req.arguments[0]  = 1;  // 1 page at a time.
+	
+		memPoolAccess(&pool, &req, &resp);
+		if(resp.status !=  OK)
+		{
+			fprintf(stderr,"Error: could not allocate memory.\n");
+			return(1);
+		}
+	
+		// save information for later use.
+		rtags[I-1] = I;
+		base_addresses[I-1] = resp.allocated_base_address;
+
+		// Write known values into the write page.
+		uint32_t J;
+		for(J=0; J < MEMPOOL_PAGE_SIZE; J++)
+		{
+			req.write_data[J] = I*J;
+		}
+
+		req.request_type = WRITE;
+		req.request_tag  = I + NPAGES;
+		req.arguments[0] = MEMPOOL_PAGE_SIZE;
+		req.arguments[1] = resp.allocated_base_address;
+
+
+
+		memPoolAccess(&pool, &req, &resp);
+		if(resp.status !=  OK)
+		{
+			fprintf(stderr,"Error: could not write into memory.\n");
+			return(1);
+		}
+
+		fprintf(stderr,"Info: wrote into page %d.\n", I);
+	}
+
+	return(0);
+}
+
+
+//
+// Note: deallocate in reverse order of allocate.
+//   return non-zero on error.
+//
+int readAndDeallocatePages(uint32_t* rtags, uint32_t* base_addresses)
+{
+	int error_flag = 0;
+	int I;
+	for(I = 1; I <= NPAGES; I++)
+	{
+		// read and check.
+		req.request_type = READ;
+		req.request_tag  = I + NPAGES;
+		req.arguments[0] = MEMPOOL_PAGE_SIZE; // 512 words.
+		req.arguments[1] = base_addresses[I-1];
+
+		memPoolAccess(&pool, &req, &resp);
+		if(resp.status !=  OK)
+		{
+			fprintf(stderr,"Error: could not read from memory.\n");
+			error_flag = 1;
+			break;
+		}
+
+		uint32_t J;
+		for(J = 0; J < MEMPOOL_PAGE_SIZE; J++)
+		{
+			if(resp.read_data[J] != I*J)
+			{
+				fprintf(stderr,"Error: read error page %d index %d\n", I, J);
+				error_flag = 1;
+			}
+		}
+
+		// Now deallocate
+		req.request_type = DEALLOCATE;
+		req.request_tag  = rtags[I-1];
+		req.arguments[0] = 1;		// 1 page to be deallocated.
+		req.arguments[1] = base_addresses[I-1];
+
+		memPoolAccess(&pool, &req, &resp);
+		if(resp.status !=  OK)
+		{
+			fprintf(stderr,"Error: could not deallocate memory.\n");
+			error_flag = 1;
+			break;
+		}
+
+		fprintf(stderr,"Info: read from page %d.\n", I);
+		
+	}
+	return(error_flag);
+}
+
+
+
+int main (int argc, char* argv[])
+{
+	_err_ = 0;
+
+	initMemPool(&pool, 1, NPAGES);	
+	
+	uint32_t request_tags[NPAGES];
+	uint32_t base_addresses[NPAGES];
+
+
+	// Allocate and fill pages
+	_err_ = allocateAndFillPages(request_tags, base_addresses) || _err_;
+
+	// read and deallocate.
+	_err_ =  readAndDeallocatePages(request_tags, base_addresses) || _err_;
+
+	if(_err_)
+		fprintf(stderr,"FAILURE.\n");
+	else
+		fprintf(stderr,"SUCCESS.\n");
+
+	return(_err_);
+}
+
+
--- C/primitives/include/tensor.h
+++ C/primitives/include/tensor.h
@@ -6,7 +6,7 @@
 typedef enum __TensorDataType {
 	u8, u16, u32, u64, 
 	i8, i16, i32, i64, 
-	float16, float32, float64
+	float8, float16, float32, float64
 } TensorDataType;
 
 typedef struct __TensorDescriptor {
@@ -30,16 +30,15 @@ typedef struct __TensorDescriptor {
 typedef struct __Tensor{
 	TensorDescriptor descriptor;
 
-	// chain of buffers will hold data in either
-	// row major or column major form as indicated
-	// in the descriptor.
+	// buffer identifier.
 	uint16_t mem_pool_identifier;
 	uint32_t mem_pool_buffer_pointer;
+
 } Tensor;
 
 
 // start
-void createTensor (uint32_t ndim, uint32_t* dims, TensorDataType dt, uint16_t mempool);
+void createTensor (uint32_t ndim, uint32_t* dims, TensorDataType dt, uint16_t mempool, Tensor* result);
 
 
 #endif

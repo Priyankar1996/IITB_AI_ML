c598deaf175880f4e2b16cd538e443e72f9d4d9a
spedar <46629325+spedar@users.noreply.github.com>
2021-06-13 01:09:36 +0530

    Add files via upload

---
+++ students/Deep/include/genRandomTensor.h
@@ -0,0 +1,25 @@
+#define __genRandomTensor_h___
+
+#include "mempool.h"
+#include "tensor.h"
+
+typedef enum {
+	mersenne_Twister, // Used in python
+	wichmann_Hill,
+	philox,
+	combRecursive,
+	threefry,
+	none
+	
+} RngType;
+
+
+//    Random: Generate an n-dimensional tensor of a given shape with pseudo random numbers
+//    generates a tensor with pseudo-random values as elements.  
+//
+//
+//    First create the result tensor with empty data,
+//    Pass the pointer to this function.
+//    The function fills the data and returns.
+//
+void genRandomTensor(uint32_t seed, RngType t, Tensor* result);

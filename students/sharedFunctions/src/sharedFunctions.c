4a627d652857034f3a368d83a60f19d0de10f57b
Aman <amandhammani2@gmail.com>
2021-07-22 17:05:41 +0530

    Bug fix in sharedFunctions

--- students/sharedFunctions/src/sharedFunctions.c
+++ students/sharedFunctions/src/sharedFunctions.c
@@ -13,19 +13,19 @@ uint32_t getSizeOfTensor(Tensor *T)
 void fillArrayFromTensor(Tensor *T,  int size, void *array)
 {
 	assert(size > 0);
-	MemPoolRequest *req;
-	MemPoolResponse *resp;
+	MemPoolRequest req;
+	MemPoolResponse resp;
 	TensorDescriptor desc = T->descriptor;
 	uint8_t dsize = sizeofTensorDataInBytes(desc.data_type);
-	req->request_type = READ;
-	req->arguments[2] = 1;
+	req.request_type = READ;
+	req.arguments[2] = 1;
 	int32_t temp_var = size;
 	while (temp_var > 0)
 	{
-		req->arguments[0] = ((temp_var*dsize/8 >= MAX_SIZE_OF_REQUEST_IN_WORDS) ? MAX_SIZE_OF_REQUEST_IN_WORDS : 1 - (-temp_var*dsize/8));
-		req->arguments[1] = T->mem_pool_buffer_pointer*MEMPOOL_PAGE_SIZE + (size - temp_var)*dsize/8;
+		req.arguments[0] = ((temp_var*dsize/8 >= MAX_SIZE_OF_REQUEST_IN_WORDS) ? MAX_SIZE_OF_REQUEST_IN_WORDS : 1 - (-temp_var*dsize/8));
+		req.arguments[1] = T->mem_pool_buffer_pointer*MEMPOOL_PAGE_SIZE + (size - temp_var)*dsize/8;
 		memPoolAccess(T->mem_pool_identifier,req,resp);
-		if (resp->status == NOT_OK)
+		if (resp.status == NOT_OK)
 		{
 			fprintf(stderr,"Mempool read error. Called from read()");
 			exit(-1);
@@ -33,7 +33,7 @@ void fillArrayFromTensor(Tensor *T,  int size, void *array)
 		uint32_t num_iterations = ((temp_var*dsize/8 >= MAX_SIZE_OF_REQUEST_IN_WORDS) ? MAX_SIZE_OF_REQUEST_IN_WORDS*8/dsize : temp_var);
 		for (int i = 0; i < num_iterations; i++)
 		{
-			copyTensorEntry(&desc,array,i+size-temp_var,resp->read_data,i);
+			copyTensorEntry(&desc,array,i+size-temp_var,resp.read_data,i);
 		}
 		temp_var -= num_iterations;
 	}
@@ -42,26 +42,26 @@ void fillArrayFromTensor(Tensor *T,  int size, void *array)
 void fillTensorFromArray(Tensor *T, int size, void *array)
 {
 	assert(size > 0);
-	MemPoolRequest *req;
-	MemPoolResponse *resp;
+	MemPoolRequest req;
+	MemPoolResponse resp;
 	TensorDescriptor desc = T->descriptor;
 	uint8_t dsize = sizeofTensorDataInBytes(desc.data_type);
-	req->request_type = WRITE;
-	req->arguments[2] = 1;
+	req.request_type = WRITE;
+	req.arguments[2] = 1;
 	int32_t temp_var = size;
-	req->arguments[1] = T->mem_pool_buffer_pointer;
+	req.arguments[1] = T->mem_pool_buffer_pointer;
 	while (temp_var > 0)
 	{
-		req->arguments[0] = ((temp_var*dsize/8 >= MAX_SIZE_OF_REQUEST_IN_WORDS) ? MAX_SIZE_OF_REQUEST_IN_WORDS : 1 - (-temp_var*dsize/8));
-		req->arguments[1] = T->mem_pool_buffer_pointer*MEMPOOL_PAGE_SIZE + (size - temp_var)*dsize/8;
+		req.arguments[0] = ((temp_var*dsize/8 >= MAX_SIZE_OF_REQUEST_IN_WORDS) ? MAX_SIZE_OF_REQUEST_IN_WORDS : 1 - (-temp_var*dsize/8));
+		req.arguments[1] = T->mem_pool_buffer_pointer*MEMPOOL_PAGE_SIZE + (size - temp_var)*dsize/8;
 		uint32_t num_iterations = ((temp_var*dsize/8 >= MAX_SIZE_OF_REQUEST_IN_WORDS) ? MAX_SIZE_OF_REQUEST_IN_WORDS*8/dsize : temp_var);
 
 		for (int i = 0; i < num_iterations; i++)
 		{
-			copyTensorEntry(&desc,req->write_data,i,array,i+size-temp_var);
+			copyTensorEntry(&desc,req.write_data,i,array,i+size-temp_var);
 		}
 		memPoolAccess(T->mem_pool_identifier,req,resp);
-		if (resp->status == NOT_OK)
+		if (resp.status == NOT_OK)
 		{
 			fprintf(stderr,"Mempool write error. Called from write()");
 			exit(-1);

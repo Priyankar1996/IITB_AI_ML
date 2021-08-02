daa2f249eba6a1b638e84ed496e93727c162bcd0
Priyankar Sarkar <priyansrk@gmail.com>
2021-08-02 14:53:23 +0530

    Added functionality for StandardIO.

---
+++ students/priyankar/include/readWriteTensorsFromStandardIO.h
@@ -0,0 +1,40 @@
+#ifndef _readWriteTensorsFromStandardIO_h____
+#define _readWriteTensorsFromStandardIO_h____
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "mempool.h"
+#include "tensor.h"
+
+
+// ASSUMPTIONS:
+//      1. t has the tensor's description available in it.
+//      2. filename is the name of the dataset's csv file.         
+// SUMMARY:
+//      Reads data from the csv file and writes them into the 
+//      memory-pool.
+// SIDE-EFFECTS:
+//      Mempool is modified to indicate that it has allocated 
+//      this data to the tensor.
+//      Values in the memory pool are not modified.
+// RETURN VALUES:
+//      0 on Success, 1 on Failure.
+int readTensorFromFile(char *filename, Tensor *t);
+
+// ASSUMPTIONS:
+//      1. t has the tensor's description available in it.
+//      2. filename is the name of the csv-file where the 
+//         resultant data will be stored.
+// SUMMARY:
+//      Reads a tensor from the memory-pool and writes them into
+//      the csv file.
+// SIDE-EFFECTS:
+//      NA
+// RETURN VALUES:
+//      0 on Success, 1 on Failure.
+int writeTensorToFile(char *filename, Tensor *t);
+// Can we assume the writen Tensor into FILE will be data-labels only?
+#endif

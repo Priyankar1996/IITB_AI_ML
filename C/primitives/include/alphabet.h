dd7a439b93a50bc4e942c827db4048bbafe4d72a
Madhav Pandurang Desai <madhav@ee.iitb.ac.in>
2021-08-18 19:19:14 +0530

    Updated.

---
+++ C/primitives/include/alphabet.h
@@ -0,0 +1,68 @@
+#ifndef _alphabet_include____
+#define _alphabet_include____
+
+#include <tensor.h>
+#include <mempool.h>
+
+
+// ASSUMPTIONS:
+//      1. t has the tensor's description available in it.
+//      2. mp is the pointer to mempool to be used by createTensor.
+// SUMMARY:
+//      createTensor allocates spaces for the tensor from the specified
+//      mempool.
+// SIDE-EFFECTS:
+//      Mempool is modified to indicate that it has allocated this data
+//      to the tensor.
+//      Tensor *t is modified with the memory location to where
+//      it is allocated.
+//      Values in the memory pool are not modified.
+// RETURN VALUES:
+//      0 on Success, 1 on Failure.
+int createTensor(Tensor *t, MemPool *mp);
+
+// ASSUMPTIONS:
+//      1. t has the tensor's description available in it.
+// SUMMARY:
+//      destroyTensor deallocates the pages containing the tensor  
+//      from the specified mempool in a FIFO manner.
+// SIDE-EFFECTS:
+//      Mempool is modified to indicate increment in number of
+//      available free pages.
+//      Values in the memory pool are modified.
+// RETURN VALUES:
+//      0 on Success, 1 on Failure.
+int destroyTensor(Tensor *t);
+
+
+// ASSUMPTIONS:
+//      1. t has the tensor's description available in it.
+//      2. initial_value is the pointer to the initial value to be
+//	   stored in the tensor.     
+// SUMMARY:
+//      initializeTensor fills up the memory allocated to the tensor
+//      with an initial value.
+// SIDE-EFFECTS:
+//      Mempool is modified to indicate a write operation on it.
+//      Values in the memory pool are modified.
+// RETURN VALUES:
+//       0 on Success, 1 on Failure.
+int initializeTensor(Tensor *t, void* initial_value);
+
+
+// ASSUMPTIONS:
+//      1. src has the source tensor's description available in it.
+//      2. dest has the destination tensor's description available 
+//         in it.
+// SUMMARY:
+//      copyTensor reads data from src tensor and writes them 
+//      to dest tensor.
+// SIDE-EFFECTS:
+//      The dest mempool is modified indicating a write operation.
+//      Values in the dest mempool are modified.
+// RETURN VALUES:
+//      0 on Success, 1 on Failure.
+int copyTensor(Tensor *src, Tensor *dest);
+
+
+#endif

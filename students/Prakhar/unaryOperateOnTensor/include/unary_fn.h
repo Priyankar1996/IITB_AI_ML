cbd0386d08ae9c849d8eb32ddc521f43dff70857
Prakhar Diwan <diwanprakhar@gmail.com>
2021-08-22 20:55:35 +0530

    added sigmoid to unaryOperateOnTensor (out-of place)
    - works for i8, i16, i32, i64, float32, float64
    - as of now, the datatype of input and output tensors to the unary functions is same. (modifications to be made)

--- students/Prakhar/unaryOperateOnTensor/include/unary_fn.h
+++ students/Prakhar/unaryOperateOnTensor/include/unary_fn.h
@@ -16,7 +16,8 @@ typedef enum {
 	EXP,
 	RELU,
 	SQUARE,
-    ABSOLUTE
+    ABSOLUTE,
+	SIGMOID
 } Operation;
 
 // Datatypes supported include: u8, u16, u32, u64, i8, i16, i32, i64, float32, float64.

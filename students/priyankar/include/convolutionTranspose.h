d96224580f11e4c34e5b5284da59669bd520c731
Priyankar Sarkar <priyansrk@gmail.com>
2021-08-25 15:00:46 +0530

    Convolution Tranpose(updated with independent functions)

--- students/priyankar/include/convolutionTranspose.h
+++ students/priyankar/include/convolutionTranspose.h
@@ -6,56 +6,40 @@
 #include <stdio.h>
 #include "mempool.h"
 #include "tensor.h"
-#include "createTensor.h"
-#include "conv.h"
 
-
+#define MIN(a,b) (((a)<(b))?(a):(b))
 #define CEILING(x,y) (((x) + (y) - 1) / (y))
-
-
-// ASSUMPTIONS:
-//      1. offset has the input tensor's element location.
-//      2. td_in has the input tensor's descriptor in it.
-//      3. td_out has the output tensor's descriptor available in it.
-//      4. k_dims stores the kernel's dimension.
-//      5. stride is a convolutional parameter.
-//      6. padding is a convolutional parameter.
-// SUMMARY:
-//      computeUpsampledInputLocation takes in the offset index of a particular 
-//      elememt in the input tensor and computes the offset of that location in 
-//      the dilated tensor.
-// SIDE-EFFECTS:
-//      N/A
-// RETURN VALUES:
-//      N/A
-uint32_t computeDilatedTensorOffset(uint32_t offset, TensorDescriptor *td_in,
-                                    TensorDescriptor *td_out, uint32_t *k_dims, 
-                                    uint32_t *stride, uint32_t padding);
 
 
 // ASSUMPTIONS:
 //      1. input has the input tensor's description available in it.
 //      2. kernel has the kernel tensor's decription available in it.
-//      3. intermediate has the dilated tensor's description in it.
-//      4. stride is a convolutional paramter.
-//      5. padding contains the value of the number of rings to be
-//         removed(integer value).
-//      6. output_padding is a convolutional parameter.
-//      7. output has the output tensor's descritption available in it.
+//      3. stride is a convolutional paramter.
+//      4. output has the output tensor's descritption available in it.
 // SUMMARY:
-//      convTranspose performs tranpose-convolution on the input by dilating
-//      the input followed by convolving it with the kernel.
+//      Dilatetensor dilates the input tensor such that the kernel placed
+//      on the top-left corner of the input tensor has atleast one element
+//      of the Tensor in it.
 // SIDE-EFFECTS:
-//      Mempool is modified to indicate that it has allocated this data
-//      to the tensor.
-//      All the tensors are modified with the memory location to where 
-//      it is allocated.
-//      Values in the memory-pool are modified.
+//      N/A
 // RETURN VALUES:
 //      0 on Success, 1 on Failure.
-int convTranspose(Tensor *input, Tensor *kernel, Tensor *intermediate, 
-                  uint32_t *stride, uint32_t *output_padding, 
-                  uint32_t padding,Tensor *output);
+int dilateTensor(Tensor *input, Tensor *kernel, uint32_t *stride, Tensor *output);
+
+// ASSUMPTIONS:
+//      1. input has the input tensor's description available in it.
+//      2. padding contains the value of the number of rings to be
+//         removed(integer value).
+//      3. output_padding is a convolutional parameter.
+//      4. output has the output tensor's descritption available in it.
+// SUMMARY:
+//      DePad tensor removes the provided number of outer rings from
+//      input tensor.
+// SIDE-EFFECTS:
+//      N/A
+// RETURN VALUES:
+//      0 on Success, 1 on Failure.
+int dePadTensor(Tensor *input, uint32_t padding, Tensor *output);
 
 #endif
 
--- students/priyankar/util/testconvTranspose.c
+++ students/priyankar/util/testconvTranspose.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
+#include "createTensor.h"
 #include "conv.h"
 #include "convolutionTranspose.h"
 #include "readWriteTensorsFromStandardIO.h"
 
 MemPool pool1;
-Tensor input,kernel,intermediate,output;
+Tensor input,kernel,intermediate_dilate,intermediate_depad,output;
 
 #define MAX_PAGES 10
 
@@ -16,7 +17,7 @@ int main()
     int _err_ = 0;
     initMemPool(&pool1,1,MAX_PAGES);
     uint32_t stride[2]={2,2}, padding[4] = {0,0,0,0};
-    uint8_t val = 1,val1 = 0;
+    uint8_t val = 3,val1 = 0;
 
     input.descriptor.data_type = 0;
     input.descriptor.row_major_form = 1;
@@ -27,33 +28,34 @@ int main()
 
     kernel.descriptor.data_type = 0;
     kernel.descriptor.row_major_form = 1;
-    kernel.descriptor.number_of_dimensions = 3;
+    kernel.descriptor.number_of_dimensions = 2;
     kernel.descriptor.dimensions[0] = 2;
     kernel.descriptor.dimensions[1] = 2;
     kernel.descriptor.dimensions[2] = 1;
 
-	intermediate.descriptor.data_type = 0;
-    intermediate.descriptor.row_major_form = 1;
-    intermediate.descriptor.number_of_dimensions = 3;
-    intermediate.descriptor.dimensions[0] = 7;
-    intermediate.descriptor.dimensions[1] = 7;
-    intermediate.descriptor.dimensions[2] = 1;
+	intermediate_dilate.descriptor.data_type = 0;
+    intermediate_dilate.descriptor.row_major_form = 1;
+    intermediate_dilate.descriptor.number_of_dimensions = 3;
+    intermediate_dilate.descriptor.dimensions[0] = 7;
+    intermediate_dilate.descriptor.dimensions[1] = 7;
+    intermediate_dilate.descriptor.dimensions[2] = 1;
 
-    output.descriptor.data_type = 0;
-    output.descriptor.row_major_form = 1;
-    output.descriptor.number_of_dimensions = 3;
-    output.descriptor.dimensions[0] = 6;
-    output.descriptor.dimensions[1] = 6;
-    output.descriptor.dimensions[2] = 1;
+    intermediate_depad.descriptor.data_type = 0;
+    intermediate_depad.descriptor.row_major_form = 1;
+    intermediate_depad.descriptor.number_of_dimensions = 3;
+    intermediate_depad.descriptor.dimensions[0] = 5;
+    intermediate_depad.descriptor.dimensions[1] = 5;
+    intermediate_depad.descriptor.dimensions[2] = 1;
 
     _err_ = createTensor(&input,&pool1)  ||
             createTensor(&kernel,&pool1) ||
-			createTensor(&intermediate,&pool1) ||
-            createTensor(&output,&pool1)  ||
+			createTensor(&intermediate_dilate,&pool1) ||
+            createTensor(&intermediate_depad,&pool1)  ||
             initializeTensor(&input,&val) ||
             initializeTensor(&kernel,&val) || _err_;
 
-    _err_ = convTranspose(&input,&kernel,&intermediate,stride,padding,0,&output) || 
-			writeTensorToFile("util/sample_csv/transConv.csv", &intermediate) ||
+    _err_ = dilateTensor(&input,&kernel,stride,&intermediate_dilate) || 
+            dePadTensor(&intermediate_dilate,1,&intermediate_depad) ||
+			writeTensorToFile("util/sample_csv/transConv.csv", &intermediate_dilate) ||
             _err_;
 }

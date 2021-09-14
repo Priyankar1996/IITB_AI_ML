df0428341ac2b566d858834fcf598fa97b79c068
AmanD15 <amandhammani2@gmail.com>
2021-09-14 18:51:50 +0530

    Update

--- C/modules/util/Test_Aman.c
+++ C/modules/util/Test_Aman.c
@@ -7,15 +7,23 @@
 
 
 int main(){
-
+    int num_dim = 0;
     int num_iters = 0;
     Tensor T[2*num_iters];
     Tensor K;
     Tensor S[2*num_iters];
-    printf("Yes\n");
 
+    int pad[num_dim];
+    int stride[num_dim];
     for (int i = 0; i < num_iters; i++){
-        // convTensors(&T[num_iters], &K, &S[num_iters] ,0 ,0 );
+        convTensors(&T[i], &K, &S[i] ,stride,pad );
+        unaryOperateOnTensor_inplace(&S[i], 2);
+        maxPoolOfTensors(&S[i], &T[i+1],2, 2,  2, stride, 0);
     }
+    for (int i = num_iters; i < 2*num_iters; i++){
+        dilateTensor(&T[i], &K, stride,  &S[i]);
+        unaryOperateOnTensor_inplace(S[i], 5);
+        
+    }    
     return 0;
 }

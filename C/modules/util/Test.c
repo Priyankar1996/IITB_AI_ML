8458d6c7240147a645cdc0ffb25a1c8b522601e4
AmanD15 <amandhammani2@gmail.com>
2021-09-16 16:52:14 +0530

    Updated Test.c

--- C/modules/util/Test.c
+++ C/modules/util/Test.c
@@ -2,17 +2,13 @@
 #include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include "createTensor.h"
-#include "convolutionTranspose.h"
-#include "conv.h"
-#include "readWriteTensorsFromStandardIO.h"
-#include "maxPoolOfTensors.h"
-#include "concat.h"
-#include "unary_inplace_fn.h"
+#include "mempool.h"
+#include "tensor.h"
+
 
 int main(){
-    int num_dim = 0;
-    int num_iters = 0;
+    int num_dim = 3;
+    int num_iters = 1;
     MemPool pool;
     Tensor T[2*num_iters];
     Tensor K;
@@ -29,19 +25,19 @@ int main(){
     for (int i = 0; i < 2*num_iters; i++)
     {
         T[i].descriptor.data_type = float32;
-        T[i].descriptor.number_of_dimensions = 3;
+        T[i].descriptor.number_of_dimensions = num_dim;
         T[i].descriptor.row_major_form = 0;
     }
 
     for (int i=0; i < 3*num_iters; i++)
     {
         S[i].descriptor.data_type = float32;
-        S[i].descriptor.number_of_dimensions = 3;
+        S[i].descriptor.number_of_dimensions = num_dim;
         S[i].descriptor.row_major_form = 0;
     }
 
     K.descriptor.data_type = float32;
-    K.descriptor.number_of_dimensions = 3;
+    K.descriptor.number_of_dimensions = num_dim;
     K.descriptor.row_major_form = 0;
     K.descriptor.dimensions[0] = 3;
     K.descriptor.dimensions[1] = 3;
@@ -60,9 +56,11 @@ int main(){
     for (int i = num_iters; i < 2*num_iters; i++){
          // There should be another loop for tensor->dimension[2] as all the operations are on 2D.
         _err_ = dilateTensor(&T[i], &K, stride,  &S[i]) ||
-                dePadTensor(&S[i],pad_deconv,&S[2*num_iters + i]) ||
-                convTensors(&S[2*num_iters + i],&K,&T[i+1],stride_deconv,pad_deconv ) || _err_;
+                dePadTensor(&S[i],pad_deconv,&S[num_iters + i]) ||
+                convTensors(&S[num_iters + i],&K,&T[i+1],stride_deconv,pad_deconv ) || _err_;
         unaryOperateOnTensor_inplace(S[i], 5);
-    }    
+    }
+    if (_err_)
+        fprintf(stderr,"Something's wrong. Please check.");
     return 0;
 }

41cf7c8cce32c741cdc428089d23844b559c99e3
spedar <46629325+spedar@users.noreply.github.com>
2021-09-25 01:22:22 +0530

    Added some files

---
+++ students/Deep/include/tb_utils.h
@@ -0,0 +1,23 @@
+#ifndef TB_UTILS
+#define TB_UTILS
+
+// #include "../../../C/primitives/include/alphabet.h"
+#include "mempool.h"
+#include "tensor.h"
+#include "createTensor.h"
+//#include <math.h>
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
+// fills elements of tensor with 
+// incremental data if offset != -1
+// 0 if offset == -1
+void fillTensorValues (Tensor* t, uint32_t num_elems, double offset, MemPoolRequest* req, MemPoolResponse* resp );
+
+// prints 2 dimensional tensor 
+// need to take care of datatype manually
+void print2dTensor(Tensor* a, MemPoolRequest* req, MemPoolResponse* resp);
+
+// #include "tb_utils.c"
+
+#endif

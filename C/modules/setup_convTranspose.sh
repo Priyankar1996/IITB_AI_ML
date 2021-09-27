a444c02ac668e3cecea5cf3c462a8d132e6e2b1d
Priyankar Sarkar <priyansrk@gmail.com>
2021-09-28 00:40:57 +0530

    Updated

--- C/modules/setup_convTranspose.sh
+++ C/modules/setup_convTranspose.sh
@@ -1,3 +1,4 @@
+mkdir -p bin
 gcc -o obj/readWriteTensors.o -c -g src/readWriteTensorsFromStandardIO.c -I include/ -I ../mempool/include/ -I ../primitives/include/
 gcc -o obj/conv.o -c -g -std=c99 src/conv.c -lm -I include/ -I ../mempool/include/ -I ../primitives/include/
 gcc -o obj/convTranspose.o -c -g -std=c99 src/convolutionTranspose.c -I include/ -I ../mempool/include/ -I ../primitives/include/ -I include/ 

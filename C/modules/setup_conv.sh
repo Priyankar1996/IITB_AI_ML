ce1e2e18039537c170fb6291741eceba7e5ab24c
spedar <46629325+spedar@users.noreply.github.com>
2021-09-27 17:14:30 +0530

    Add files via upload

---
+++ C/modules/setup_conv.sh
@@ -0,0 +1,4 @@
+gcc -o obj/readWriteTensors.o -c -g src/readWriteTensorsFromStandardIO.c -I include/ -I ../mempool/include/ -I ../primitives/include/
+gcc -o obj/conv.o -c -g src/conv.c -lm -I include/ -I ../mempool/include/ -I ../primitives/include/
+gcc -o obj/testconv.o -c -g util/conv_tb.c -I include/ -I ../mempool/include/ -I ../primitives/include/ -I include/ 
+gcc -o bin/conv obj/conv.o obj/createTensor.o obj/mempool.o obj/tensor.o obj/readWriteTensors.o obj/testconv.o

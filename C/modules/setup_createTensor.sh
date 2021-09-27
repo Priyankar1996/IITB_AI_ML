abb3f67b41d384bf1c144baa63a79547ae1d2095
Priyankar Sarkar <priyansrk@gmail.com>
2021-09-27 10:36:53 +0530

    Updated

--- C/modules/setup_createTensor.sh
+++ C/modules/setup_createTensor.sh
@@ -2,6 +2,7 @@
 gcc -o obj/mempool.o -c -g -I ../mempool/include ../mempool/src/mempool.c 
 gcc -o obj/tensor.o -c -g -I ../mempool/include -I ../primitives/include ../primitives/src/tensor.c
 gcc -o obj/createTensor.o -c -g -I src/ -I include/ src/createTensor.c -lm -I ../mempool/include/ -I ../primitives/include/
+gcc -o obj/readWriteTensors.o -c -g src/readWriteTensorsFromStandardIO.c -I include/ -I ..//mempool/include/ -I ../primitives/include/
 gcc -o obj/testCreateTensor.o -g -c util/testCreateTensor.c -I ../mempool/include/ -I ../primitives/include/ -I include/
-gcc -o bin/testCreateTensor obj/mempool.o obj/tensor.o obj/createTensor.o obj/testCreateTensor.o 
+gcc -o bin/testCreateTensor obj/mempool.o obj/tensor.o obj/createTensor.o obj/readWriteTensors.o obj/testCreateTensor.o 
 # Use the ar comand to put all the .o files (other than the test benches into your archive.

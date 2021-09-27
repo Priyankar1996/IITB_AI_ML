db95b14d9a19ca5b48b0c4f3c456090fca1cfa4d
Ajinkya Sangram Raghuwanshi <65677801+AjinkyaRaghuwanshi@users.noreply.github.com>
2021-09-28 01:32:21 +0530

    Update zero_pad_tensor.c

--- C/modules/src/zero_pad_tensor.c
+++ C/modules/src/zero_pad_tensor.c
@@ -12,6 +12,8 @@ void read_write_data(Tensor *src, uint32_t scale_factor, uint32_t constant, Tens
 uint32_t i[64] = { };
 uint32_t ind[64] = { };
 uint32_t desti[64] = { };
+
+// Function for creating the indices
 void funcn(Tensor *src, Tensor *dest,int n,int scale_factor, int constant){
     // printf("\n func entered !!!");
     if (n == 0){
@@ -42,6 +44,7 @@ void zeropad(Tensor *src, uint32_t scale_factor, uint32_t constant, Tensor *dest
     // printf("%d\n", sizeof(i)/sizeof(i[0]));
     
     printf("\n func started !!!");
+    // Creating indices and reading as well as writing the tensor
     funcn(src,dest,src->descriptor.number_of_dimensions,scale_factor,constant);
     printf("\n func ended !!!");
 

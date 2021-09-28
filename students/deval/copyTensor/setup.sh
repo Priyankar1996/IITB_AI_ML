7ec1396775239d6e7960618eef889e2cddc9c046
deval1812 <30732768+deval1812@users.noreply.github.com>
2021-09-28 13:17:34 +0530

    Update setup.sh

--- students/deval/copyTensor/setup.sh
+++ students/deval/copyTensor/setup.sh
@@ -8,4 +8,3 @@ gcc -o obj/copyTensor.o -c -g -I src/ -I include/ src/copyTensor.c -lm -I ../../
 
 gcc -o obj/test_copyTensor.o -g -c utils/test_copyTensor.c -I ../../priyankar/include/ -I ../../../C/mempool/include/ -I ../../../C/primitives/include/ -I include/
 gcc -o bin/test_copyTensor obj/mempool.o obj/tensor.o obj/copyTensor.o obj/createTensor.o obj/test_copyTensor.o
-$SHELL

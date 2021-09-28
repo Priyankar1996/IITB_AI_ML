1058992e500b3f4beed5eef0354140dc2f41ead2
deval1812 <30732768+deval1812@users.noreply.github.com>
2021-09-28 13:31:20 +0530

    Update setup.sh

--- students/deval/convolution/setup.sh
+++ students/deval/convolution/setup.sh
@@ -5,8 +5,8 @@ gcc -o obj/tensor.o -c -g -I ../../../C/mempool/include -I ../../../C/primitives
 gcc -o obj/createTensor.o -c -g ../../priyankar/src/createTensor.c -lm -I ../../priyankar/include/ -I ../../../C/mempool/include/ -I ../../../C/primitives/include/
 gcc -o obj/readWriteTensors.o -c -g ../../priyankar/src/readWriteTensorsFromStandardIO.c -I ../../priyankar/include/ -I ../../../C/mempool/include/ -I ../../../C/primitives/include/
 
-gcc -o obj/new_conv.o -g -c -std=c99 src/new_conv.c -lm -I include/ -lm -I ../../../C/mempool/include/ -I ../../../C/primitives/include/
+gcc -o obj/conv.o -g -c -std=c99 src/conv.c -lm -I include/ -lm -I ../../../C/mempool/include/ -I ../../../C/primitives/include/
 gcc -o obj/testConv.o -c -g -std=c99 utils/conv_tb.c -I include/ -I ../../priyankar/include/ -I ../../../C/mempool/include/ -I ../../../C/primitives/include/ 
 
-gcc -o bin/testConvExec obj/mempool.o obj/tensor.o obj/createTensor.o obj/readWriteTensors.o obj/new_conv.o obj/testConv.o
+gcc -o bin/testConvExec obj/mempool.o obj/tensor.o obj/createTensor.o obj/readWriteTensors.o obj/conv.o obj/testConv.o
 # Use the ar comand to put all the .o files (other than the test benches into your archive.

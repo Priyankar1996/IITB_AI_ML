26d241c02f8357bb86f27e43687a52d2ee281183
spedar <46629325+spedar@users.noreply.github.com>
2021-09-28 15:16:09 +0530

    Add files via upload

--- students/Deep/src/conv.c
+++ students/Deep/src/conv.c
@@ -1,113 +1,18 @@
-#include "../include/conv.h"
+//AUTHOR: 	DEEP SATRA
+//          DEPT. OF ELECTRICAL ENGINEERING, IIT-BOMBAY.
 
-void convHelper(const int64_t *ker_data, const int64_t *img_data, 
-				const TensorDataType dtype,
-				const int32_t *ker_start_ds_offset, 
-				const int32_t *img_start_ds_offset, 
-				const uint32_t *num_ele_to_operate,
-				union ConvResult *res){
-	switch (dtype)
-	{
-		int i = 0;
-		case u8: 
-			for(i = 0; i < *num_ele_to_operate; i++){
-				res->res_u8 += *((uint8_t*)ker_data+*ker_start_ds_offset+i)
-					* *((uint8_t*)img_data+*img_start_ds_offset+i);
-			}
-			break;
+#include "conv.h"
+#include "tensor.h"
+#include "mempool.h"
+#include "createTensor.h"
 
-		case u16: 
-			for(i = 0; i < *num_ele_to_operate; i++){
-				res->res_u16 += *((uint16_t*)ker_data+*ker_start_ds_offset+i)
-					* *((uint16_t*)img_data+*img_start_ds_offset+i);
-			}
-			break;
-			
-		case u32: 
-			for(i = 0; i < *num_ele_to_operate; i++){
-				res->res_u32 += *((uint32_t*)ker_data+*ker_start_ds_offset+i)
-					* *((uint32_t*)img_data+*img_start_ds_offset+i);
-			}
-			break;
-			
-		case u64: 
-			for(i = 0; i < *num_ele_to_operate; i++){
-				res->res_u64 += *(ker_data+*ker_start_ds_offset+i)
-					* *(img_data+*img_start_ds_offset+i);
-			}
-			break;
-			
-
-		case i8:
-			for(i = 0; i < *num_ele_to_operate; i++){
-				res->res_i8 += *((int8_t*)ker_data+*ker_start_ds_offset+i)
-					* *((int8_t*)img_data+*img_start_ds_offset+i);
-			}
-			break;
-			
-		case i16: 
-			for(i = 0; i < *num_ele_to_operate; i++){
-				res->res_i16 += *((int16_t*)ker_data+*ker_start_ds_offset+i)
-					* *((int16_t*)img_data+*img_start_ds_offset+i);
-			}
-			break;
-			
-		case i64: 
-			for(i = 0; i < *num_ele_to_operate; i++){
-				res->res_i64 += *((int64_t*)ker_data+*ker_start_ds_offset+i)
-					* *((int64_t*)img_data+*img_start_ds_offset+i);
-			}
-			break;
-			
-		case i32: 
-			for(i = 0; i < *num_ele_to_operate; i++){
-				res->res_i32 += *((int32_t*)ker_data+*ker_start_ds_offset+i)
-					* *((int32_t*)img_data+*img_start_ds_offset+i);
-			}
-			break;
-			
-		
-		// case f8:
-		// for(int i = 0; i < *num_ele_to_operate; i++){
-		//     res->res_u8 += *((uint8_t*)ker_data+*ker_start_ds_offset+i)
-		//         * *((uint8_t*)img_data+*img_start_ds_offset+i);
-		// }
-		// break;
-			
-		// case f16: 
-		// for(int i = 0; i < *num_ele_to_operate; i++){
-		//     res->res_u8 += *((uint8_t*)ker_data+*ker_start_ds_offset+i)
-		//         * *((uint8_t*)img_data+*img_start_ds_offset+i);
-		// }
-		// break;
-			
-		case float32: 
-			for(i = 0; i < *num_ele_to_operate; i++){
-				res->res_f32 += *((float*)ker_data+*ker_start_ds_offset+i)
-					* *((float*)img_data+*img_start_ds_offset+i);
-			}
-			break;
-			
-		case float64: 
-			for(i = 0; i < *num_ele_to_operate; i++){
-				res->res_f64 += *((double*)ker_data+*ker_start_ds_offset+i)
-					* *((double*)img_data+*img_start_ds_offset+i);
-			}
-			break;
-		
-		default:
-			fprintf(stderr,"ERROR: CONV HELPER INPUTS DATATYPE NOT FOUND\n");
-			break;
-	}
-}
-
-int convTensors(Tensor *in_img, Tensor *kernel, Tensor *out_img, 
-			const int stride[2], const int padding[4]){
+int convTensors(Tensor *in_img, Tensor *kernel, Tensor *out_img, MemPool *mpool, 
+			const int stride[3], const int padding[6]){
 	
 	fprintf(stderr,"INSIDE CONV\n");
 	
-	MemPoolRequest reqKer, reqImg;
-	MemPoolResponse respKer, respImg;
+	MemPoolRequest reqKer, reqImg, reqOut;
+	MemPoolResponse respKer, respImg, respOut;
 
 	TensorDescriptor td_in, td_ker, td_out;
 	td_in = in_img->descriptor;
@@ -131,14 +36,15 @@ int convTensors(Tensor *in_img, Tensor *kernel, Tensor *out_img,
 	uint32_t in_C = td_in.dimensions[2];
 	uint32_t ker_H = td_ker.dimensions[0];
 	uint32_t ker_W = td_ker.dimensions[1];
-	uint32_t out_H = (td_in.dimensions[0] + padding[0] + padding[1] 
-					- td_ker.dimensions[0])/stride[0] + 1;
-	uint32_t out_W = (td_in.dimensions[1] + padding[2] + padding[3] 
-					- td_ker.dimensions[1])/stride[1] + 1;
-	uint32_t out_C = 1; //td_ker.dimensions[td_ker.number_of_dimensions - 1];
+	uint32_t ker_C = td_ker.dimensions[2];
+
+	// Initialise output sizes
+	uint32_t out_H = (in_H + padding[0] + padding[1] - ker_H)/stride[0] + 1;
+	uint32_t out_W = (in_W + padding[2] + padding[3] - ker_W)/stride[1] + 1;
+	uint32_t out_C = (in_C + padding[4] + padding[5] - ker_C)/stride[2] + 1; //td_ker.dimensions[td_ker.number_of_dimensions - 1];
 	
 	out_img->descriptor.row_major_form = is_row_major;
-	out_img->descriptor.number_of_dimensions = 2;
+	out_img->descriptor.number_of_dimensions = 3;
 	out_img->descriptor.data_type = td_in.data_type;
 	out_img->descriptor.dimensions[0] = out_H;
 	out_img->descriptor.dimensions[1] = out_W;
@@ -146,139 +52,156 @@ int convTensors(Tensor *in_img, Tensor *kernel, Tensor *out_img,
 	td_out = out_img->descriptor;
 	
 	int num_dim_ker = td_ker.number_of_dimensions;
-	// int num_ele_per_filt_per_ch = td_ker.dimensions[0]*td_ker.dimensions[1];
-	int num_ele_per_filt = td_ker.dimensions[0]*td_ker.dimensions[1]
-							*td_ker.dimensions[2];
-	// for(int i = 2; i < num_dim_ker - 1; i++){
-	//     num_ele_ker *= td_ker.dimensions[i];
-	// }
+	// Number of multiplications to get one value
+	int num_ele_per_filt = ker_W*ker_H*ker_C;
+	// Number of output values
+	int num_ele_out = out_W*out_H*out_C;
+
 	uint32_t data_size = sizeofTensorDataInBytes(td_in.data_type);
 
-	int32_t ker_word_offset, img_word_offset;
-	int32_t ker_start_ds_offset, img_start_ds_offset; //ds is data size
-	union ConvResult res;
-	uint32_t size_so_far; //in bytes
-	uint64_t word_to_store;
-	int ii, ij; //image coords
+	// Number of words to read to get kernel data
+	uint32_t num_words_ker = CEILING(num_ele_per_filt*data_size,8) ;
+	
+	// Currently only i64 and float64
+	reqKer.request_type = READ ;
+	reqKer.arguments[0] = num_words_ker;
+	reqKer.arguments[1] = kernel->mem_pool_buffer_pointer;
+	reqKer.arguments[2] = 1; // stride = 1 as pointwise
 
-	if(td_in.row_major_form == 1){
-		// for(int kc = 0; kc < out_C; kc++){
-			size_so_far = 0;
-			word_to_store = 0;
-			int oi = 0;
-			for(oi = 0; oi < out_H; oi++){
-				int oj = 0;
-				for(oj = 0; oj < out_C; oj++){
-					/*
-						output index i, j
-						=> input window's top left position is:
-							i*SH, j*SW w/o padding
-						obtain o/p value at (i, j) ele by ele of kernel.
-					*/
-					/*
-					{
-						// switch (td_in.data_type)
-						// {
-						//     case u8: res.res_u8 = 0; break;
-						//     case u16: res.res_u16 = 0; break;
-						//     case u32: res.res_u32 = 0; break;
-						//     case u64: res.res_u64 = 0; break;
-
-						//     case i8: res.res_i8 = 0; break;
-						//     case i16: res.res_i16 = 0; break;
-						//     case i32: res.res_i32 = 0; break;
-						//     case i64: res.res_i64 = 0; break;
-							
-						//     // case f8: res.res_f8 = 0; break;
-						//     // case f16: res.res_f16 = 0; break;
-						//     case float32: res.res_f32 = 0; break;
-						//     case float64: res.res_f64 = 0; break;
-							
-						//     default:
-						//         fprintf(stderr,"ERROR: CONV INPUTS DATATYPE NOT FOUND\n");
-						//         return -1;
-						//         break;
-						// }
-					}*/
-					res.res_u64 = 0u;
-					int ki = 0 ;
-					for(ki = 0; ki < ker_H; ki++){
-						int kj = 0;
-						for(kj = 0; kj < ker_W; kj++){
-							reqKer.request_type = READ;
-							reqKer.request_tag = 1; //TODO check
-							reqKer.arguments[0] = CEILING(in_C * data_size, 8) + 1;
-								//read 1 extra word in case data we need is not word aligned
-							ker_word_offset = (ki * ker_W + kj) * in_C * data_size / 8;
-							reqKer.arguments[1] = kernel->mem_pool_buffer_pointer 
-								+ ker_word_offset;
-							reqKer.arguments[2] = 1; // stride = 1 as pointwise
-							memPoolAccess((MemPool*)kernel->mem_pool_identifier,&reqKer,&respKer);
-							if(respKer.status == NOT_OK){
-								fprintf(stderr,"ERROR: Read Kernel Tensor Failed in Conv\n");
-								return -1;
-							}
-							ker_start_ds_offset = (ki * ker_W + kj) * in_C 
-													- 8 * ker_word_offset / data_size;
-							
-							ii = oi*stride[0]; ij = oj*stride[1];
-							reqImg.request_type = READ;
-							reqImg.request_tag = 1; //TODO check
-							reqImg.arguments[0] = CEILING(in_C * data_size, 8) + 1;
-							img_word_offset = (ii * in_W + ij) * in_C * data_size / 8;
-							reqImg.arguments[1] = kernel->mem_pool_buffer_pointer 
-								+ img_word_offset;
-							reqImg.arguments[2] = 1; // stride = 1 as pointwise
-							memPoolAccess((MemPool*)kernel->mem_pool_identifier,&reqImg,&respImg);
-							if(respImg.status == NOT_OK){
-								fprintf(stderr,"ERROR: Mempool Read Image Tensor Failed in Conv\n");
-								return -1;
-							}
-							img_start_ds_offset = (ii * in_W + ij) * in_C 
-													- 8 * img_word_offset / data_size;
-							convHelper(respKer.read_data, respImg.read_data,
-								td_ker.data_type, &ker_start_ds_offset, &img_start_ds_offset,
-								&in_C, &res
-							);
-						}//end kj
-					}//end ki
-					word_to_store |= (res.res_u64 << (size_so_far*8));
-					size_so_far += data_size;
-					if(size_so_far == MEMPOOL_WORD_SIZE){
-						//write word_to_store
-						reqImg.request_type = WRITE;
-						reqImg.arguments[0] = 1;
-						reqImg.arguments[1] = out_img->mem_pool_buffer_pointer +
-												(oi * out_W + oj) * data_size / 8;
-						reqImg.write_data[0] = word_to_store;
-						memPoolAccess((MemPool*)out_img->mem_pool_identifier, &reqImg, &respImg);
-						if (respImg.status == NOT_OK){
-							fprintf(stderr,"ERROR: Mempool Write Image Tensor Failed in Conv\n");
-							return -1;
-						}
-						size_so_far = 0;
-					}
-				}//end j loop
-			}//end i loop
-		// }//end kc loop
-		if(size_so_far > 0){
-			//write word_to_store
-			reqImg.request_type = WRITE;
-			reqImg.arguments[0] = 1;
-			reqImg.arguments[1] = out_img->mem_pool_buffer_pointer +
-									((out_H-1) * out_W + out_C - 1) * data_size / 8;
-			reqImg.write_data[0] = word_to_store;
-			memPoolAccess((MemPool*)out_img->mem_pool_identifier, &reqImg, &respImg);
-			if (respImg.status == NOT_OK){
-				fprintf(stderr,"ERROR: Mempool Write Image Tensor Failed in Conv\n");
-				return -1;
-			}
-			size_so_far = 0;
-		}
-	}//end row major
-	else{//column major
-		fprintf(stderr,"ERROR: Conv not available for COLUMN major yet\n");
+	memPoolAccess((MemPool *)kernel->mem_pool_identifier,&reqKer,&respKer);
+	if(respKer.status == NOT_OK){
+		fprintf(stderr,"ERROR: Read Kernel Tensor Failed in Conv\n");
 		return -1;
-	}//end column major
-	return 0; //SUCCESS
+	}
+
+	int flag = createTensorAtHead(out_img, mpool);
+	//uint8_t init_val = 0;
+	//flag = flag + initializeTensor(&out_img, &init_val);
+	if(flag != 0){
+		fprintf(stderr,"ERROR: Initialising output Tensor Failed in Conv\n");
+		return -1;
+	}
+
+	// Number of pages to write 
+	uint8_t loops = CEILING(num_ele_out, MAX_SIZE_OF_REQUEST_IN_WORDS);
+	uint8_t I=0; // Keeps track of page number 
+	uint32_t done=0; // Keeps track of element to write
+	for(I=0;I<loops;I++){
+		// Words to write in current page
+		uint32_t curr_writes = (I == loops - 1)? num_ele_out % MAX_SIZE_OF_REQUEST_IN_WORDS : MAX_SIZE_OF_REQUEST_IN_WORDS ;		
+		uint32_t J = 0 ; // Keeps track of word to be written in current page
+		switch(td_in.data_type){
+			case i64:
+				for(J=0; J<curr_writes; J++){
+					uint8_t oc = done / (out_H*out_W); // C coord of output
+					uint8_t oh = (done-oc*out_W*out_H)/(out_W), ow = done - oc*out_H*out_W - oh*out_W; // H, W coord of output
+					int32_t iws = ow*stride[1] - padding[2], iwe = iws + ker_W -1; 
+					int32_t ihs = oh*stride[0] - padding[0], ihe = ihs + ker_H -1; 
+					int32_t ics = oc*stride[2] - padding[4], ice = ics + ker_C -1; 
+
+					iws = (iws<0)?0:iws ; iwe = (iwe>=in_W)? in_W-1 : iwe ; 
+					ihs = (ihs<0)?0:ihs ; ihe = (ihe>=in_H)? in_H-1 : ihe ;
+					ics = (ics<0)?0:ics ; ice = (ice>=in_C)? in_C-1 : ice ;
+
+					//printf("iws:%d iwe:%d ihs:%d ihe:%d ics:%d ice:%d\n",iws,iwe,ihs,ihe,ics,ice);
+					uint64_t ans = 0, id = 0;
+					uint32_t tc;
+					for (tc=ics;tc<=ice;tc++){
+						uint32_t th ;
+						for (th=ihs;th<=ihe;th++){
+							uint32_t tw ;
+							for(tw=iws;tw<=iwe;tw++){
+								uint32_t indices[] = {th,tw,tc};
+								uint32_t loc = getTensorEntryIndexOffset(&td_in, indices) ;
+								reqImg.request_type = READ ;
+								reqImg.arguments[0] = 1 ; //Read 1 by 1
+								reqImg.arguments[1] = in_img->mem_pool_buffer_pointer + loc ;
+								reqImg.arguments[2] = 1;
+								memPoolAccess((MemPool *)in_img->mem_pool_identifier, &reqImg, &respImg);
+								if(respImg.status !=  OK)
+								{
+									fprintf(stderr,"Error: could not read Img from memory.\n");
+									break;
+								}
+								//printf("Multiplying %d with {%d,%d,%d}\n",id,tw,th,tc);
+								ans += respKer.read_data[id]*respImg.read_data[0];	
+								id++;								
+							}
+						}
+					}
+					
+					reqOut.write_data[J + I*MAX_SIZE_OF_REQUEST_IN_WORDS] = ans;
+					done++;
+				}
+
+				break;
+			case float64:
+				for(J=0; J<curr_writes; J++){
+					uint8_t oc = done / (out_H*out_W);
+					uint8_t oh = (done-oc*out_W*out_H)/(out_H), ow = done - oc*out_H*out_W - oh*out_H; 
+					int32_t iws = ow*stride[1] - padding[2], iwe = iws + ker_W -1; 
+					int32_t ihs = oh*stride[0] - padding[0], ihe = ihs + ker_H -1; 
+					int32_t ics = oc*stride[2] - padding[4], ice = ics + ker_C -1; 
+
+					iws = (iws<0)?0:iws ; iwe = (iwe>=in_W)? in_W-1 : iwe ; 
+					ihs = (ihs<0)?0:ihs ; ihe = (ihe>=in_H)? in_H-1 : ihe ;
+					ics = (ics<0)?0:ics ; ice = (ice>=in_C)? in_C-1 : ice ;
+
+					//printf("iws:%d iwe:%d ihs:%d ihe:%d ics:%d ice:%d\n",iws,iwe,ihs,ihe,ics,ice);
+					union udouble {
+						double d ;
+						uint64_t u;
+					} ans, rk, ri ;
+					ans.d=0;
+					uint64_t id = 0;
+					uint32_t tc;
+					for (tc=ics;tc<=ice;tc++){
+						uint32_t th ;
+						for (th=ihs;th<=ihe;th++){
+							uint32_t tw ;
+							for(tw=iws;tw<=iwe;tw++){
+								uint32_t indices[] = {th,tw,tc};
+								uint32_t loc = getTensorEntryIndexOffset(&td_in, indices) ;
+								reqImg.request_type = READ ;
+								reqImg.arguments[0] = 1 ; //Read 1 by 1
+								reqImg.arguments[1] = in_img->mem_pool_buffer_pointer + loc ;
+								reqImg.arguments[2] = 1;
+								memPoolAccess((MemPool *)in_img->mem_pool_identifier, &reqImg, &respImg);
+								if(respImg.status !=  OK)
+								{
+									fprintf(stderr,"Error: could not read Img from memory.\n");
+									break;
+								}
+								//printf("Multiplying %d with {%d,%d,%d}\n",id,tw,th,tc);
+								rk.u = respKer.read_data[id] ;
+								ri.u = respImg.read_data[0] ;
+								//printf("ker: %f img: %f\n",rk.d, ri.d );
+								ans.d += rk.d * ri.d ;	
+								id++;								
+							}
+						}
+					}
+					//printf("ans: %lf\n",ans.d);
+					reqOut.write_data[J + I*MAX_SIZE_OF_REQUEST_IN_WORDS] = ans.u;
+					done++;
+				}
+
+				break;
+			default:
+				printf("Conv not ready yet for other data types\n");
+				return -1 ;
+		}
+		reqOut.request_type = WRITE ;
+		reqOut.request_tag = I + 1 ;
+		reqOut.arguments[0] = curr_writes ;
+		reqOut.arguments[1] = out_img->mem_pool_buffer_pointer + I*MAX_SIZE_OF_REQUEST_IN_WORDS;
+		reqOut.arguments[2] = 1 ;
+		memPoolAccess((MemPool *)out_img->mem_pool_identifier, &reqOut, &respOut);
+		if(respOut.status !=  OK)
+		{
+			fprintf(stderr,"Error: could not write Convolved tensor into memory.\n");
+			return -1;
+		}
+	}
+	return 0; 	
 }

9bda3a1f705f7783497ca309bc29eed48065320d
Nayan Barhate <180070037@iitb.ac.in>
2021-10-11 01:09:21 +0530

    concat fixed

--- C/modules/setup_concat.sh
+++ C/modules/setup_concat.sh
@@ -5,9 +5,6 @@ gcc -o obj/tensor.o -c -g -std=c99 -I ../mempool/include -I ../primitives/includ
 gcc -o obj/createTensor.o -c -g -std=c99 -I src/ -I include/ src/createTensor.c  -I ../mempool/include/ -I ../primitives/include/ -lm
 gcc -o obj/readWriteTensors.o -c -g -std=c99 src/readWriteTensorsFromStandardIO.c -I include/ -I ../mempool/include/ -I ../primitives/include/
 gcc -o obj/concat.o -c -g -std=c99 src/concat.c -I include/ -I ../mempool/include/ -I ../primitives/include/ -lm
-# gcc -o obj/tb_utils.o -c -g -std=c99 src/tb_utils.c -I include/  -lm 
 
 gcc -o obj/test_concat.o -c -g -std=c99 util/concat_tb.c -I include/ -I ../mempool/include/ -I ../primitives/include/ -I src/
-gcc -o bin/test_concat obj/concat.o obj/test_concat.o obj/createTensor.o obj/mempool.o obj/tensor.o obj/readWriteTensors.o -lm #obj/tb_utils.o
-
-./bin/test_concat
+gcc -o bin/test_concat obj/concat.o obj/test_concat.o obj/createTensor.o obj/mempool.o obj/tensor.o obj/readWriteTensors.o -lm 

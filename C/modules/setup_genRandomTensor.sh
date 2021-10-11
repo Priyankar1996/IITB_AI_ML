b881f4060ab1386177095539255b168bb6551ea4
spedar <46629325+spedar@users.noreply.github.com>
2021-10-11 23:48:51 +0530

    Add files via upload

--- C/modules/setup_genRandomTensor.sh
+++ C/modules/setup_genRandomTensor.sh
@@ -1,4 +1,4 @@
-gcc -o obj/mtwist.o -c -g include/mtwist.c -lm -I include/
-gcc -o obj/genRandomTensor.o -c -g src/genRandomTensor.c -lm -I include/ -I ../mempool/include/ -I ../primitives/include/
-gcc -o obj/testgenRandom.o -c -g util/test_genRandomTensor.c -I include/ -I ../mempool/include/ -I ../primitives/include/ -I include/ 
+gcc -o obj/mtwist.o -c -g ../mtwist/src/mtwist.c -lm -I ../mtwist/include/
+gcc -o obj/genRandomTensor.o -c -g src/genRandomTensor.c -lm -I include/ -I ../mempool/include/ -I ../primitives/include/ -I ../mtwist/include
+gcc -o obj/testgenRandom.o -c -g util/test_genRandomTensor.c -I include/ -I ../mempool/include/ -I ../primitives/include/ -I include/ -I ../mtwist/include
 gcc -o bin/genRandom obj/genRandomTensor.o obj/createTensor.o obj/mempool.o obj/tensor.o obj/testgenRandom.o obj/mtwist.o

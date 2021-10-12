e3562f546c47646eb5d4c6e92d242b9dbcc9a70d
Ajinkya Sangram Raghuwanshi <65677801+AjinkyaRaghuwanshi@users.noreply.github.com>
2021-10-12 12:19:20 +0530

    Add files via upload

--- C/modules/setup_zero_pad_tensor.sh
+++ C/modules/setup_zero_pad_tensor.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 mkdir -p bin
 echo 'started'
-gcc -o obj/mempool.o -c -g -I ../mempool/include ../mempool/src/mempool.c -lm
+gcc -o obj/mempool.o -c -g -I ../mempool/include ../mempool/src/mempool.c
 echo 'done 1st'
-gcc -o obj/tensor.o -c -g -I ../mempool/include -I ../primitives/include ../primitives/src/tensor.c -lm
+gcc -o obj/tensor.o -c -g -I ../mempool/include -I ../primitives/include ../primitives/src/tensor.c
 echo 'done 2nd'
-gcc -o obj/createTensor.o -c -g -I src/ -I include/ src/createTensor.c -lm -I ../mempool/include/ -I ../primitives/include/ -lm
+gcc -o obj/createTensor.o -c -g -I src/ -I include/ src/createTensor.c -lm -I ../mempool/include/ -I ../primitives/include/
 echo 'done 3rd'
-gcc -o obj/zero_pad_tensor.o -g -c src/zero_pad_tensor.c -I ../mempool/include/ -I ../primitives/include/ -I ../include/ -I include/ -lm
+gcc -o obj/zero_pad_tensor.o -g -c src/zero_pad_tensor.c -I ../mempool/include/ -I ../primitives/include/ -I ../include/ -I include/
 echo 'done 4th'
-gcc -o obj/zero_pad_tensor_test.o -g -c util/zero_pad_tensor_test.c -lm -I ../mempool/include/ -I ../primitives/include/  -I include/ -lm
+gcc -o obj/zero_pad_tensor_test.o -g -c util/zero_pad_tensor_test.c -I ../mempool/include/ -I ../primitives/include/  -I include/
 echo 'done 5th'
-gcc -o bin/zero_pad_tensor_test obj/mempool.o obj/tensor.o obj/createTensor.o obj/zero_pad_tensor.o obj/zero_pad_tensor_test.o -lm
+gcc -o bin/zero_pad_tensor_test obj/mempool.o obj/tensor.o obj/createTensor.o obj/zero_pad_tensor.o obj/zero_pad_tensor_test.o 
 echo 'done 6th'

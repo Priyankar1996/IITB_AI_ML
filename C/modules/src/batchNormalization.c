cb15ce903e282dfd0ebfb84a1256f62ef63f6268
AmanD15 <amandhammani2@gmail.com>
2021-10-19 09:37:54 +0530

    Updated batchNorm

--- C/modules/src/batchNormalization.c
+++ C/modules/src/batchNormalization.c
@@ -87,18 +87,56 @@ void batchNormalization(Tensor *input, Tensor *beta, Tensor *gamma,
         case float8:{break;}
         case float16:{break;}
         case float32:{
-			float beta_arr[x], gamma_arr[x], mm_arr[x], mv_arr[x], input_arr[num_elems_input], resultant[num_elems_input];
+			float beta_arr[x], gamma_arr[x], mm_arr[x], mv_arr[x];
 			fillArrayFromTensor(beta, num_elems, beta_arr);
 			fillArrayFromTensor(gamma, num_elems, gamma_arr);
 			fillArrayFromTensor(moving_mean, num_elems, mm_arr);
 			fillArrayFromTensor(moving_variance,num_elems,mv_arr);
-			fillArrayFromTensor(input,num_elems_input,input_arr);
-			for(int i=0;i<num_elems_input;i++)
+			MemPoolRequest req1,req2;
+			MemPoolResponse resp1,resp2;
+			TensorDescriptor desc = input->descriptor;
+			uint8_t dsize = sizeofTensorDataInBytes(desc.data_type);
+			req1.request_type = READ;
+			req1.arguments[2] = 1;
+			int32_t temp_var = num_elems_input;
+
+			req2.request_type = WRITE;
+			req2.arguments[2] = 1;
+			while (temp_var > 0)
 			{
-				int count = i%x;
-				resultant[i] = gamma_arr[count]*(input_arr[i]-mm_arr[count])/(mv_arr[count] + epsilon) + beta_arr[count];
+				float in_arr[2*MAX_SIZE_OF_REQUEST_IN_WORDS],out_arr[2*MAX_SIZE_OF_REQUEST_IN_WORDS];
+				req1.arguments[0] = ((temp_var*dsize/8 >= MAX_SIZE_OF_REQUEST_IN_WORDS) ? MAX_SIZE_OF_REQUEST_IN_WORDS : 1 - (-temp_var*dsize/8));
+				req1.arguments[1] = input->mem_pool_buffer_pointer*MEMPOOL_PAGE_SIZE + (num_elems_input - temp_var)*dsize/8;
+				memPoolAccess((MemPool*)input->mem_pool_identifier,&req1,&resp1);
+				if (resp1.status == NOT_OK)
+				{
+					fprintf(stderr,"Mempool read error. Called from read()");
+					exit(-1);
+				}
+				uint32_t num_iterations = ((temp_var*dsize/8 >= MAX_SIZE_OF_REQUEST_IN_WORDS) ? MAX_SIZE_OF_REQUEST_IN_WORDS*8/dsize : temp_var);
+				for (int i = 0; i < num_iterations; i++)
+				{
+					copyTensorEntry(&desc,in_arr,i,resp1.read_data,i);
+				}
+				req2.arguments[0] = ((temp_var*dsize/8 >= MAX_SIZE_OF_REQUEST_IN_WORDS) ? MAX_SIZE_OF_REQUEST_IN_WORDS : 1 - (-temp_var*dsize/8));
+				req2.arguments[1] = input->mem_pool_buffer_pointer*MEMPOOL_PAGE_SIZE + (num_elems_input - temp_var)*dsize/8;
+				for (int i = 0; i < num_iterations; i++)
+				{
+					int count = (num_elems_input + i - temp_var)%x;
+					out_arr[i] = gamma_arr[count]*(in_arr[i]-mm_arr[count])/(mv_arr[count] + epsilon) + beta_arr[count];
+				}
+				for (int i = 0; i < num_iterations; i++)
+				{
+					copyTensorEntry(&desc,req2.write_data,i,out_arr,i);
+				}
+				memPoolAccess((MemPool*)input->mem_pool_identifier,&req2,&resp2);
+				if (resp2.status == NOT_OK)
+				{
+					fprintf(stderr,"Mempool write error. Called from write()");
+					exit(-1);
+				}
+				temp_var -= num_iterations;
 			}
-			fillTensorFromArray(input,num_elems_input,resultant);
 			break;}
         case float64:{break;}
     }

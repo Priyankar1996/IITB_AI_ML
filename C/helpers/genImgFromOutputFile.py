95fe5046999e05564a68459a4041f1990139aff7
deval1812 <30732768+deval1812@users.noreply.github.com>
2021-10-26 16:40:32 +0530

    Update genImgFromOutputFile.py

--- C/helpers/genImgFromOutputFile.py
+++ C/helpers/genImgFromOutputFile.py
@@ -1,18 +1,39 @@
 import numpy as np
 from PIL import Image as im
 import csv
+import sys
 
-def genImgFromOutputFile(csv_file_name):
-  #load file to numpy array
-  matrix = np.loadtxt(csv_file_name,skiprows=4,delimiter=',')
-  #reshape the matrix
-  reshaped_matrix = np.reshape(matrix,(matrix.shape[0],matrix.shape[1]))  #resize Image object
-  #round off float numbers
-  reshaped_matrix = np.round(reshaped_matrix)
-  #convert pixel values to 8 bit color
-  reshaped_matrix = reshaped_matrix.astype("uint8")
-  #save image in png format
-  data = im.fromarray(reshaped_matrix)
-  data.save("../../students/deval/convolution/bin/result.png")
 
-genImgFromOutputFile("../../students/deval/convolution/bin/r.csv")
+def genInputFileFromImg(data_type,row_major,is_grayscale,image_file_name,csv_file_name):
+  #Load image variable
+	img = im.open(image_file_name)
+  #Convert to numpy array
+	imageToMatrice = np.asarray(img)
+	original_stdout = sys.stdout
+  #open csv file in write mode
+	with open(csv_file_name, 'w') as f:
+		sys.stdout = f
+    #print console output to file
+		print(data_type)
+		print(row_major)
+		print(imageToMatrice.ndim)
+		if(is_grayscale):
+	#Uncomment for space separated data
+    			#print(imageToMatrice.shape[0],imageToMatrice.shape[1],1)
+    	#Uncomment for comma separated data
+			print(imageToMatrice.shape[0],",",imageToMatrice.shape[1],",",1,sep='')
+			for i in range(imageToMatrice.shape[0]):
+				for j in range(imageToMatrice.shape[1]):
+        	#Uncomment for comma separated data
+					print(imageToMatrice[i][j][0])
+			sys.stdout = original_stdout
+		else:
+			print(imageToMatrice.shape[0],",",imageToMatrice.shape[1],",",3,sep='')
+			for i in range(imageToMatrice.shape[0]):
+				for j in range(imageToMatrice.shape[1]):
+        	#Uncomment for space separated data
+        				#print(imageToMatrice[i][j][0],imageToMatrice[i][j][1],imageToMatrice[i][j][2])
+        	#Uncomment for comma separated data
+					print(imageToMatrice[i][j][0],",",imageToMatrice[i][j][1],",",imageToMatrice[i][j][2],sep='')
+			sys.stdout = original_stdout
+genInputFileFromImg(5,1,1,"../../students/deval/convolution/bin/cat2_original.jpg","../../students/deval/convolution/bin/a.csv")

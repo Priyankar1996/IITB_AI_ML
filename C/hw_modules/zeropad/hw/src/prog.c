b36ba22b6b772b60dbe6ed12436e7d000e46e959
AjinkyaRaghuwanshi <ajinkyaraghuwanshi@gmail.com>
2022-01-25 22:24:35 +0530

    Added the Hardware version.... but to be completed

---
+++ C/hw_modules/zeropad/hw/src/prog.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdint.h>
+#include <Pipes.h>
+#include "pipeHandler.h"
+#include "sized_tensor.h"
+#include "zero_pad.h"
+
+#ifndef SW
+void __loop_pipelining_on__(uint32_t pipeline_depth, uint32_t buffering, uint32_t full_rate);
+#endif
+
+SizedTensor_16K input,output;
+uint16_t pad;
+
+#define __get4xi16__(element) ({\
+	element = read_uint16 ("ZeroPad_input_pipe");\
+	element = (element << 16) + read_uint16 ("ZeroPad_input_pipe");\
+	element = (element << 16) + read_uint16 ("ZeroPad_input_pipe");\
+	element = (element << 16) + read_uint16 ("ZeroPad_input_pipe");\
+})
+
+#define __set4xi16__(addr) ({\
+	uint64_t element = output.data_array[addr];\
+	uint16_t out_data[4];\
+	out_data[3] = element & 0xFFFF;\
+	element>>=16;\
+	out_data[2] = element & 0xFFFF;\
+	element>>=16;\
+	out_data [1]= element & 0xFFFF;\
+	element>>=16;\
+	out_data[0] = element & 0xFFFF;\
+	write_uint16 ("ZeroPad_output_pipe",out_data[0]);\
+	write_uint16 ("ZeroPad_output_pipe",out_data[1]);\
+	write_uint16 ("ZeroPad_output_pipe",out_data[2]);\
+	write_uint16 ("ZeroPad_output_pipe",out_data[3]);\
+})
+
+void ReadInputInfo()
+{
+    fprintf(stderr,"Starting test configure\n");
+
+    input.descriptor.descriptor.data_type = i16;
+    input.descriptor.descriptor.row_major_form = 1;
+    input.descriptor.descriptor.number_of_dimensions = 3;
+    int i;
+    for(i = 0;i < input.descriptor.descriptor.number_of_dimensions;i++){
+        input.descriptor.descriptor.dimensions[i] = read_uint16 ("ZeroPad_input_pipe");
+    }
+
+    pad = read_uint16 ("ZeroPad_input_pipe");
+    
+    uint64_t input_size = __NumberOfElementsInSizedTensor__(input);
+
+    for(i = 0; i < (input_size >> 2); i ++)
+    {
+        uint64_t element;
+        // __get4xi16__ reads 4 16-bit numbers from
+		// maxpool_input_pipe, and packs them into 
+		// a 64 bit number
+        __get4xi16__(element);
+
+        input.data_array[i] = element;
+    }
+    fprintf(stderr,"Ending test_configure\n");
+}
+
+void sendOutput()
+{
+    fprintf(stderr,"Starting test send\n");
+    write_uint16("ZeroPad_output_pipe",output.descriptor.descriptor.dimensions[0]);
+    write_uint16("ZeroPad_output_pipe",output.descriptor.descriptor.dimensions[1]);
+    write_uint16("ZeroPad_output_pipe",output.descriptor.descriptor.dimensions[2]);
+    uint64_t size = __NumberOfElementsInSizedTensor__(output);
+    int i;
+    for (i = 0; i < size; i++){
+        __set4xi16__(i);
+    }
+    fprintf(stderr,"Ending test_send\n");
+}
+
+void zeropad_thread()
+{
+    ReadInputInfo();
+    __zero_pad__(input,pad,output);
+    sendOutput();
+}
---
+++ C/hw_modules/zeropad/hw/src/zero_pad.h
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <float.h>
+#include "sized_tensor.h"
+
+#define __dim0__(dest) ({dest.descriptor.descriptor.dimensions[0];})
+#define __dim1__(dest) ({dest.descriptor.descriptor.dimensions[1];})
+#define __dim2__(dest) ({dest.descriptor.descriptor.dimensions[2];})
+// The function "zeropadtensor" creates a new tensor for zero padding 
+// It creates it from the src tensor by reading and then writing the 
+// destination tensor according to scale factor provded. 
+
+// ASSUMPTIONS:
+//      1. source (src) tensor is already declared with its description
+//      2. destination (dest) tensor is already declared taking into scale factor into account
+//         , with its description.
+// SUMMARY:
+//      Zero padding of the tensor : Adds outer layer of constant (mostly 0) to the pre-existing 
+//      src tensor to create the dest tensor, in all dimensions 
+// SIDE-EFFECTS:
+//      The data stored in the destination tensor is changes according to the src and the 
+//      constant as well as the scale factor bby which we want to pad the src tensor
+// ARGUEMENTS:
+//      scr :- pointer to the source tensor
+//      scale_factor :- The factor of layer which is added to each dimension for zero padding
+//      dest :- pointer to the destination tensor
+// RETURN VALUES:
+//      NA
+#define __zero_pad__(src, scale_factor,dest)  ({\
+    int n1 = src.descriptor.descriptor.dimensions[0];\
+    int n2 = src.descriptor.descriptor.dimensions[1];\
+    int n3 = src.descriptor.descriptor.dimensions[2];\
+    __SizedTensor_1M dest2;\
+    dest2.descriptor.descriptor.data_type = i16;\
+    dest2.descriptor.descriptor.number_of_dimensions = 3;\
+    dest2.descriptor.descriptor.dimensions[0] = dest.descriptor.descriptor.dimensions[0];\
+    dest2.descriptor.descriptor.dimensions[1] = dest.descriptor.descriptor.dimensions[1];\
+    dest2.descriptor.descriptor.dimensions[2] = dest.descriptor.descriptor.dimensions[2];\
+    dest2.descriptor.descriptor.row_major_form = 1;\
+    dest2.descriptor.tensor_size = (dest.descriptor.descriptor.dimensions[0])*(dest.descriptor.descriptor.dimensions[1])*(dest.descriptor.descriptor.dimensions[2]);\
+    int size=(n1*n2*n3);\
+    int pad=scale_factor;\
+    int idx   = 0;\
+	int width = 0;\
+	for(int i=0;i<n1;i++)\
+	{\
+		for(int j=0;j<n2;j++ )\
+		{\
+			for(int k=0;k<n3;k++)\
+			{\
+				*(((int16_t*)dest2.data_array) + (n1+2*pad+pad)+i+j*(n1+2*pad)+(k*(n1+2*pad)*(n2+2*pad))) = *(((int16_t*)src.data_array) + (k+n3*j+n2*n3*i));\
+			}\
+		}\
+	}\
+    int index=0;\
+    int jump_matrix = ((n1+2*pad)*(n2+2*pad));\
+    int row_jump = (n1+2*pad);\
+    int column_jump = (n2+2*pad);\
+    for(int i=0;i<n1+2*pad;i++)\
+	{\
+		for(int j=0;j<n2+2*pad;j++)\
+		{\
+			for(int k=0;k<n3;k++)\
+			{\
+                *(((int16_t*)dest.data_array) + index) = *(((int16_t*)dest2.data_array) +  k*jump_matrix +  j*(n1+2*pad) + i );\
+		        printf("%d ",*(((int16_t*)dest.data_array) + index));\
+			    index = index + 1 ;\
+			}\
+		}\
+	}\
+})

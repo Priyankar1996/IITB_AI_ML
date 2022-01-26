938da68689235e18210f49e78bbbb0bb04463054
Priyankar Sarkar <priyansrk@gmail.com>
2022-01-26 13:28:59 +0530

    Updated

--- C/hw_modules/convolution_transpose/include/convolution_transpose_improved.h
+++ C/hw_modules/convolution_transpose/include/convolution_transpose_improved.h
@@ -95,9 +95,9 @@
     conv_output_indices[output.descriptor.descriptor.number_of_dimensions-1] = conv_indices[output.descriptor.descriptor.number_of_dimensions-1];\
     for (mm=0;mm<output.descriptor.descriptor.number_of_dimensions-1;mm++) {\
         if(conv_output_indices[mm] < 0 || conv_output_indices[mm] >= output.descriptor.descriptor.dimensions[mm])\
-            flag = flag || 1;\
+            flag = flag | 1;\
         else\
-            flag = flag || 0;\
+            flag = flag | 0;\
     }\
     conv_output_flag = flag ? -1 : __GetTensorEntryIndexOffset__(output,conv_output_indices);\
     flag = 0;\
@@ -124,6 +124,49 @@
             conv_offset = __CheckConvTransposedTensor__(offset,input,output,kernel,stride,padding);\
             if(conv_offset>=0)\
                 *((__dt__*)output.data_array + conv_offset) = bytes[kl];\
+        }\
+    }\
+})
+
+#define __ConvTranspose2__(input,kernel,stide,padding,output) ({\
+    __UpdateOutputDescriptorConvTransTensors__(input,kernel,stride,padding,output);\
+    uint16_t datasize = __SizeOfTensorDataInBytes__(input.descriptor.descriptor.data_type),\
+            num_elems_input = __NumberOfElementsInSizedTensor__(input),\
+            conv_output_indices[3],flag=0,conv_output_flag;\
+    static uint16_t input_indices[3];\
+    for(uint16_t i=0;i<CEILING(num_elems_input*datasize,8);i++){\
+        uint64_t read_data = input.data_array[i];\
+        uint16_t bytes[4];\
+        bytes[0] = read_data & 0xFFFF;\
+        bytes[1] = (read_data >> 16) & 0xFFFF;\
+        bytes[2] = (read_data >> 32) & 0xFFFF;\
+        bytes[3] = (read_data >> 48) & 0xFFFF;\
+        for(uint16_t kl=0;kl<__dt_size__;kl++) {\
+            for(uint16_t mm = 0;mm < output.descriptor.descriptor.number_of_dimensions-1;mm++)\
+                conv_output_indices[mm] = input_indices[mm] * stride[mm] + kernel.descriptor.descriptor.dimensions[mm+1] - padding - 1;\
+            conv_output_indices[output.descriptor.descriptor.number_of_dimensions-1] = input_indices[output.descriptor.descriptor.number_of_dimensions-1];\
+            for(uint16_t mm=0;mm<output.descriptor.descriptor.number_of_dimensions-1;mm++) {\
+                if(conv_output_indices[mm] < 0 || conv_output_indices[mm] >= output.descriptor.descriptor.dimensions[mm])\
+                    flag = flag | 1;\
+                else\
+                    flag = flag | 0;\
+            }\
+            conv_output_flag = flag ? -1 : __GetTensorEntryIndexOffset__(output,conv_output_indices);\
+            flag = 0;\
+            if(conv_output_flag>=0) {\
+                output.data_array[conv_output_flag >> 2] += (uint64_t)bytes[kl] << 16*(conv_output_flag & 3);\
+            }\
+            if((input_indices[2] == (input.descriptor.descriptor.dimensions[2] - 1))) {\
+                input_indices[2] = 0;\
+                if((input_indices[1] == (input.descriptor.descriptor.dimensions[1] - 1))) {\
+                    input_indices[1] = 0;\
+                    input_indices[0]++;\
+                }\
+                else\
+                    input_indices[1]++;\
+            }\
+            else\
+                input_indices[2]++;\
         }\
     }\
 })

3c9fab16d679f17e276201c5bff74475f269b3ef
= <=>
2022-02-08 10:31:16 +0530

    Added improved version of maxpool

---
+++ C/hw_modules/maxPool/hw_4_l2/src/prog.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdint.h>
+#include <Pipes.h>
+#include "pipeHandler.h"
+#include "sized_tensor.h"
+#include "maxPoolOfTensors.h"
+
+SizedTensor_16K T,B;
+uint16_t length, stride;
+
+#define __get4xi16__(element) ({\
+	element = read_uint16 ("maxpool_input_pipe");\
+	element = (element << 16) + read_uint16 ("maxpool_input_pipe");\
+	element = (element << 16) + read_uint16 ("maxpool_input_pipe");\
+	element = (element << 16) + read_uint16 ("maxpool_input_pipe");\
+})
+
+#define __set4xi16__(addr) ({\
+	uint64_t element = B.data_array[addr];\
+	uint16_t out_data[4];\
+	out_data[3] = element & 0xFFFF;\
+	element>>=16;\
+	out_data[2] = element & 0xFFFF;\
+	element>>=16;\
+	out_data [1]= element & 0xFFFF;\
+	element>>=16;\
+	out_data[0] = element & 0xFFFF;\
+	write_uint16 ("maxpool_output_pipe",out_data[0]);\
+	write_uint16 ("maxpool_output_pipe",out_data[1]);\
+	write_uint16 ("maxpool_output_pipe",out_data[2]);\
+	write_uint16 ("maxpool_output_pipe",out_data[3]);\
+})
+
+uint64_t getRemainingElements(uint16_t ne){
+	uint64_t element = 0;
+	for (uint16_t n = 0 ; n < ne; n++){
+		element += read_uint16 ("maxpool_input_pipe");
+		element <<= 16;
+	}
+	element <<= 16*(3-ne);
+	return element;
+}
+
+void sendRemainingElements(int addr, uint16_t ne){
+	uint64_t element = B.data_array[addr];\
+	uint16_t out_data[3];\
+	element>>=16;\
+	out_data[2] = element & 0xFFFF;\
+	element>>=16;\
+	out_data [1]= element & 0xFFFF;\
+	element>>=16;\
+	out_data[0] = element & 0xFFFF;\
+	for (int n = 0; n < ne; n++)
+		write_uint16 ("maxpool_output_pipe",out_data[n]);
+}
+
+void testConfigure()
+{
+	length = read_uint16 ("maxpool_input_pipe");
+	stride = read_uint16 ("maxpool_input_pipe");
+
+	// configure the tensor T
+	T.descriptor.descriptor.data_type = u16;
+	T.descriptor.descriptor.row_major_form = 1;
+	T.descriptor.descriptor.number_of_dimensions = 3;
+	B.descriptor.descriptor.number_of_dimensions = 3;
+	int i;
+	for (i = 0;i < T.descriptor.descriptor.number_of_dimensions;i++){
+		T.descriptor.descriptor.dimensions[i] = read_uint16 ("maxpool_input_pipe");
+		B.descriptor.descriptor.dimensions[i] = read_uint16 ("maxpool_input_pipe");
+	}
+
+	// size = number of 16-bit values in data array..
+	uint64_t size = __NumberOfElementsInSizedTensor__(T);
+	for (i = 0; i < (size >> 2); i++)
+	{
+		uint64_t element;
+		// __get4xi16__ reads 4 16-bit numbers from
+		// maxpool_input_pipe, and packs them into 
+		// a 64 bit number
+ 		__get4xi16__(element);
+
+		T.data_array[i] = element;
+	}
+	if (size&3) T.data_array[i] = getRemainingElements(size&3);
+}
+
+// this sends B...
+void sendB()
+{
+	uint64_t size = __NumberOfElementsInSizedTensor__(B);
+	int i;
+	for(i=0; i < (size>>2); i++)
+	{
+		__set4xi16__(i);
+	}
+	if (size&3) sendRemainingElements(i,size&3);
+}
+
+
+void maxPool3D()
+{
+	testConfigure();	
+#ifndef SW
+	uint64_t start_time = timer();
+#endif
+	__maxPoolOfTensors3D__(T,B,stride);
+#ifndef SW
+	uint64_t stop_time = timer();
+	uint64_t elapsed_time = stop_time - start_time;
+	write_uint64("elapsed_time_pipe", elapsed_time);
+#endif
+	sendB ();
+}
---
+++ C/hw_modules/maxPool/hw_4_l2/testbench.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <time.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#include "sized_tensor.h"
+#include "maxPoolOfTensors.h"
+ 
+#ifdef SW
+#include <pipeHandler.h>
+#include <Pipes.h>
+#include <pthreadUtils.h>
+#else
+#include "vhdlCStubs.h"
+#endif
+
+#ifdef SW
+DEFINE_THREAD(maxPool3D);
+#endif
+
+SizedTensor_16K T,B;
+
+int main(int argc, char**argv){
+
+	fprintf(stderr,"Entering testbench main program\n");
+
+	srand(time(0));
+
+	FILE *file, *outFile, *octaveInFile;
+	if ((file = fopen(argv[1],"r")) == NULL){
+		fprintf(stderr,"Input File error\n");
+		exit(-1);
+	}
+	if ((outFile = fopen("COutFile.txt","w")) == NULL){
+		fprintf(stderr,"Output File error\n");
+		exit(-1);
+	}
+	char *oct = "octaveInput.txt";
+	if ((octaveInFile = fopen(oct,"w")) == NULL){
+		fprintf(stderr,"Octave Input File error\n");
+		exit(-1);
+	}
+	fprintf(stderr,"Defined and opened files\n");
+
+#ifdef SW
+	init_pipe_handler();
+	register_pipe ("maxpool_input_pipe", 2, 16, PIPE_FIFO_MODE);
+	register_pipe ("maxpool_output_pipe", 2, 16, PIPE_FIFO_MODE);
+
+	PTHREAD_DECL(maxPool3D);
+
+	PTHREAD_CREATE(maxPool3D);
+#endif
+
+	fprintf(stderr,"Reading files\n");
+
+	uint8_t rand_data;
+	fscanf(file,"%hhd",&rand_data);
+
+	#ifdef __U8
+		T.descriptor.descriptor.data_type = u8;
+		fprintf(octaveInFile,"0\n");
+	#endif
+	#ifdef __U16
+		T.descriptor.descriptor.data_type = u16;
+		fprintf(octaveInFile,"1\n");
+	#endif
+	#ifdef __U32
+		T.descriptor.descriptor.data_type = u32;
+		fprintf(octaveInFile,"2\n");
+	#endif
+	#ifdef __U64
+		T.descriptor.descriptor.data_type = u64;
+		fprintf(octaveInFile,"3\n");
+	#endif
+	#ifdef __I8
+		T.descriptor.descriptor.data_type = i8;
+		fprintf(octaveInFile,"4\n");
+	#endif
+	#ifdef __I16
+		T.descriptor.descriptor.data_type = i16;
+		fprintf(octaveInFile,"5\n");
+	#endif
+	#ifdef __I32
+		T.descriptor.descriptor.data_type = i32;
+		fprintf(octaveInFile,"6\n");
+	#endif
+	#ifdef __I64
+		T.descriptor.descriptor.data_type = i64;
+		fprintf(octaveInFile,"7\n");
+	#endif
+	#ifdef __F8
+		T.descriptor.descriptor.data_type = float8;
+		fprintf(octaveInFile,"8\n");
+	#endif
+	#ifdef __F16
+		T.descriptor.descriptor.data_type = float16;
+		fprintf(octaveInFile,"9\n");
+	#endif
+	#ifdef __F32
+		T.descriptor.descriptor.data_type = float32;
+		fprintf(octaveInFile,"10\n");
+	#endif
+	#ifdef __F64
+		T.descriptor.descriptor.data_type = float64;
+		fprintf(octaveInFile,"11\n");
+	#endif
+
+	fprintf(stderr,"Entering data to tensor\n");
+	uint16_t length,stride;
+	fscanf(file,"%hu%hu",&length,&stride);
+	fprintf(octaveInFile,"%hu\n%hu\n",length,stride);
+	write_uint16("maxpool_input_pipe",length);
+	write_uint16("maxpool_input_pipe",stride);
+
+	T.descriptor.descriptor.number_of_dimensions = 3;
+	B.descriptor.descriptor.number_of_dimensions = T.descriptor.descriptor.number_of_dimensions;
+	int i;
+	for (i = 0;i < T.descriptor.descriptor.number_of_dimensions;i++){
+		fscanf(file,"%d",&T.descriptor.descriptor.dimensions[i]);
+		fprintf(octaveInFile,"%d\n",T.descriptor.descriptor.dimensions[i]);
+		write_uint16("maxpool_input_pipe",T.descriptor.descriptor.dimensions[i]);
+	
+		if (i==2) B.descriptor.descriptor.dimensions[i] = T.descriptor.descriptor.dimensions[i];
+		else B.descriptor.descriptor.dimensions[i] = 1 + (T.descriptor.descriptor.dimensions[i]-length)/stride;
+		write_uint16("maxpool_input_pipe",B.descriptor.descriptor.dimensions[i]);
+	}
+	
+
+	uint64_t size = __NumberOfElementsInSizedTensor__(T);
+
+	int16_t temp[4];
+	for (i = 0; i < size; i++)
+	{
+		if (rand_data)	temp[i&3] = rand();	//Random data
+		else temp[i&3] = i+1;					//Sequential data
+		fprintf(octaveInFile,"%hd\n",temp[i&3]);
+		write_uint16("maxpool_input_pipe",temp[i&3]);
+		fprintf(stderr,"Sent element %d\n",i);
+		if ((i&3)==3) T.data_array[i/4] = *(uint64_t*)temp;
+	}
+	T.data_array[i/4] = *(uint64_t*)temp;
+	fclose(octaveInFile);
+
+	fprintf(stderr,"Checkpoint\n");
+	
+	fprintf(outFile,"Size of output is ");
+	for (i =0; i<B.descriptor.descriptor.number_of_dimensions;i++)
+	{
+		fprintf(outFile,"%hu ",B.descriptor.descriptor.dimensions[i]);
+	}
+	fprintf(outFile,"\n");
+	size = __NumberOfElementsInSizedTensor__(B);
+	fprintf(stderr,"Size of output is %d\n",size );
+	fprintf(stderr,"Reading back the values from hardware\n");
+	
+	int16_t val;
+	for (i = 0; i < size; i++)
+	{
+		val = read_uint16("maxpool_output_pipe");
+		fprintf(outFile,"%d %hd\n",i+1, val);
+		fprintf(stderr,"%d %hd\n",i+1, val);
+	}
+	
+	fprintf(stderr,"Read back the values from hardware\n");
+
+	fclose(file);
+	fclose(outFile);
+	int system(const char *command);
+	fprintf(stderr,"Calling reference implementation\n");
+
+	char arr[100] = "octave ../util_macro/octaveFile <";
+	strcat(arr,oct);
+	strcat(arr," >OctaveOutFile.txt\n");
+	system(arr);
+
+	printf("If no message is printed after this one, there is no error!!\n");
+#ifndef SW
+	uint64_t time_taken = read_uint64("elapsed_time_pipe");
+	fprintf(stderr,"Time taken is %lu\n",time_taken);
+#endif
+	system("cmp COutFile.txt OctaveOutFile.txt");
+
+#ifdef SW
+	PTHREAD_CANCEL(maxPool3D);
+	close_pipe_handler();
+#endif
+return 0;
+
+}

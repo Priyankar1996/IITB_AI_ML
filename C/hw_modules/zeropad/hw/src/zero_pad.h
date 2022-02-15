129bcc99c41e1d44802ffc95964691db694c7b07
AjinkyaRaghuwanshi <ajinkyaraghuwanshi@gmail.com>
2022-02-15 20:22:09 +0530

    Some changes on hw

--- C/hw_modules/zeropad/hw/src/zero_pad.h
+++ C/hw_modules/zeropad/hw/src/zero_pad.h
@@ -12,9 +12,9 @@
 void zeropad_thread();
 
 #define __I16 1
-#define __dim0__(dest) ({dest.descriptor.descriptor.dimensions[0];})
-#define __dim1__(dest) ({dest.descriptor.descriptor.dimensions[1];})
-#define __dim2__(dest) ({dest.descriptor.descriptor.dimensions[2];})
+#define __dim0__(T) ({T.descriptor.descriptor.dimensions[0];})
+#define __dim1__(T) ({T.descriptor.descriptor.dimensions[1];})
+#define __dim2__(T) ({T.descriptor.descriptor.dimensions[2];})
 
 #define __dt__ int16_t
 #define __dt_min_val__ 0x8000
@@ -25,50 +25,41 @@ void __loop_pipelining_on__(uint32_t pipeline_depth, uint32_t buffering, uint32_
 #else
 	#define __loop_pipeline_var__ {;}
 #endif
-// The function "zeropadtensor" creates a new tensor for zero padding 
-// It creates it from the src tensor by reading and then writing the 
-// destination tensor according to scale factor provded. 
 
-// ASSUMPTIONS:
-//      1. source (src) tensor is already declared with its description
-//      2. destination (dest) tensor is already declared taking into scale factor into account
-//         , with its description.
-// SUMMARY:
-//      Zero padding of the tensor : Adds outer layer of constant (mostly 0) to the pre-existing 
-//      src tensor to create the dest tensor, in all dimensions 
-// SIDE-EFFECTS:
-//      The data stored in the destination tensor is changes according to the src and the 
-//      constant as well as the scale factor bby which we want to pad the src tensor
-// ARGUEMENTS:
-//      scr :- pointer to the source tensor
-//      scale_factor :- The factor of layer which is added to each dimension for zero padding
-//      dest :- pointer to the destination tensor
-// RETURN VALUES:
-//      NA
-#define __zero_pad__(src, scale_factor,dest)  ({\
-    int n1 = src.descriptor.descriptor.dimensions[0];\
-    int n2 = src.descriptor.descriptor.dimensions[1];\
-    int n3 = src.descriptor.descriptor.dimensions[2];\
-    SizedTensor_1M dest2;\
+#define __zero_pad__(T, pad, R)  ({\
+    int n1 = T.descriptor.descriptor.dimensions[0];\
+    int n2 = T.descriptor.descriptor.dimensions[1];\
+    int n3 = T.descriptor.descriptor.dimensions[2];\
+    SizedTensor_16K dest2;\
     dest2.descriptor.descriptor.data_type = i16;\
     dest2.descriptor.descriptor.number_of_dimensions = 3;\
-    dest2.descriptor.descriptor.dimensions[0] = dest.descriptor.descriptor.dimensions[0];\
-    dest2.descriptor.descriptor.dimensions[1] = dest.descriptor.descriptor.dimensions[1];\
-    dest2.descriptor.descriptor.dimensions[2] = dest.descriptor.descriptor.dimensions[2];\
+    dest2.descriptor.descriptor.dimensions[0] = R.descriptor.descriptor.dimensions[0];\
+    dest2.descriptor.descriptor.dimensions[1] = R.descriptor.descriptor.dimensions[1];\
+    dest2.descriptor.descriptor.dimensions[2] = R.descriptor.descriptor.dimensions[2];\
     dest2.descriptor.descriptor.row_major_form = 1;\
-    dest2.descriptor.tensor_size = (dest.descriptor.descriptor.dimensions[0])*(dest.descriptor.descriptor.dimensions[1])*(dest.descriptor.descriptor.dimensions[2]);\
-    int size=(n1*n2*n3);\
-    int pad=scale_factor;\
+    dest2.descriptor.tensor_size = (R.descriptor.descriptor.dimensions[0])*(R.descriptor.descriptor.dimensions[1])*(R.descriptor.descriptor.dimensions[2]);\
+	int out_idx = -1;\
+	int16_t result_temp;\
+	int size=(n1*n2*n3);\
     int idx   = 0;\
 	int width = 0;\
 	int i, j, k;\
+	int dest2_ind, src_ind;\
 	for(i=0;i<n1;i++)\
 	{\
+		out_idx++;\
+		result_temp = 0;\
 		for(j=0;j<n2;j++ )\
 		{\
 			for(k=0;k<n3;k++)\
 			{\
-				*(((int16_t*)dest2.data_array) + (n1+2*pad+pad)+i+j*(n1+2*pad)+(k*(n1+2*pad)*(n2+2*pad))) = *(((int16_t*)src.data_array) + (k+n3*j+n2*n3*i));\
+				src_ind = (k+n3*j+n2*n3*i);\
+				uint64_t img_data = T.data_array[src_ind >> 2];\
+				uint8_t temp_img_rem = src_ind - 4*(src_ind >> 2);\
+				int16_t img_one_block = __getOneBlock__(img_data,temp_img_rem);\
+				dest2_ind = (n1+2*pad+pad)+i+j*(n1+2*pad)+(k*(n1+2*pad)*(n2+2*pad));\
+				uint8_t temp_out_rem = dest2_ind - 4*(dest2_ind >> 2);\
+				dest2.data_array[dest2_ind >> 2] = __putOneBlock__(dest2.data_array[dest2_ind >> 2],temp_out_rem,img_one_block);\
 			}\
 		}\
 	}\
@@ -76,17 +67,32 @@ void __loop_pipelining_on__(uint32_t pipeline_depth, uint32_t buffering, uint32_
     int jump_matrix = ((n1+2*pad)*(n2+2*pad));\
     int row_jump = (n1+2*pad);\
     int column_jump = (n2+2*pad);\
+	out_idx = -1;\
     for(i=0;i<n1+2*pad;i++)\
 	{\
+		out_idx++;\
+		result_temp = 0;\
 		for(j=0;j<n2+2*pad;j++)\
 		{\
 			for(k=0;k<n3;k++)\
 			{\
-                *(((int16_t*)dest.data_array) + index) = *(((int16_t*)dest2.data_array) +  k*jump_matrix +  j*(n1+2*pad) + i );\
-		        fprintf("%d ",*(((int16_t*)dest.data_array) + index));\
-			    index = index + 1 ;\
+				dest2_ind = k*jump_matrix +  j*(n1+2*pad) + i ;\
+                uint64_t img_data = dest2.data_array[dest2_ind >> 2];\
+				uint8_t temp_img_rem = dest2_ind - 4*(dest2_ind >> 2);\
+				int16_t img_one_block = __getOneBlock__(img_data,temp_img_rem);\
+				uint8_t temp_out_rem = index - 4*(index >> 2);\
+				R.data_array[index >> 2] = __putOneBlock__(R.data_array[index >> 2],temp_out_rem,img_one_block);\
+				index = index + 1 ;\
 			}\
 		}\
 	}\
+})
+#define __getOneBlock__(array64bit,position)   ({\
+	int16_t result = (int16_t)((array64bit >> (position*16)) & 0x000000000000FFFF);\
+	result;\
+})
+#define __putOneBlock__(array64bit,position,result)  ({\
+	array64bit = array64bit | (((uint64_t)result) << (position*16));\
+	array64bit;\
 })
 #endif

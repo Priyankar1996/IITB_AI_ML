c830552809cebf884d152ca1977725073dbe7b80
AjinkyaRaghuwanshi <ajinkyaraghuwanshi@gmail.com>
2022-02-22 16:41:11 +0530

    Some improvements by removing Temporary Tensor with output in time 2853 units

--- C/hw_modules/zeropad/hw2/src/zero_pad.h
+++ C/hw_modules/zeropad/hw2/src/zero_pad.h
@@ -9,6 +9,12 @@
 #include <assert.h>
 
 void zeropad3D();
+#ifndef SW
+void __loop_pipelining_on__(uint32_t pipeline_depth, uint32_t buffering, uint32_t full_rate);
+	#define __loop_pipeline_var__ __loop_pipelining_on__(15,1,1);
+#else
+	#define __loop_pipeline_var__ {;}
+#endif
 
 #define __dim0__(T) ({T.descriptor.descriptor.dimensions[0];})
 #define __dim1__(T) ({T.descriptor.descriptor.dimensions[1];})
@@ -18,61 +24,43 @@ void zeropad3D();
     int n1 = T.descriptor.descriptor.dimensions[0];\
     int n2 = T.descriptor.descriptor.dimensions[1];\
     int n3 = T.descriptor.descriptor.dimensions[2];\
-    SizedTensor_16K dest2;\
-    dest2.descriptor.descriptor.data_type = i16;\
-    dest2.descriptor.descriptor.number_of_dimensions = 3;\
-    dest2.descriptor.descriptor.dimensions[0] = R.descriptor.descriptor.dimensions[0];\
-    dest2.descriptor.descriptor.dimensions[1] = R.descriptor.descriptor.dimensions[1];\
-    dest2.descriptor.descriptor.dimensions[2] = R.descriptor.descriptor.dimensions[2];\
-    dest2.descriptor.descriptor.row_major_form = 1;\
-    dest2.descriptor.tensor_size = (R.descriptor.descriptor.dimensions[0])*(R.descriptor.descriptor.dimensions[1])*(R.descriptor.descriptor.dimensions[2]);\
-	int out_idx = -1;\
-	int16_t result_temp;\
-	int size=(n1*n2*n3);\
-    int idx   = 0;\
-	int width = 0;\
 	int i, j, k;\
-	int dest2_ind, src_ind;\
-	for(i=0;i<n1;i++)\
+	for(i = 0; i < n1; i++)\
 	{\
-		out_idx++;\
-		result_temp = 0;\
-		for(j=0;j<n2;j++ )\
-		{\
-			for(k=0;k<n3;k++)\
+		for(j = 0; j < n2; j++)\
 			{\
-				src_ind = (k+n3*j+n2*n3*i);\
-				uint64_t img_data = T.data_array[src_ind >> 2];\
-				uint8_t temp_img_rem = src_ind - 4*(src_ind >> 2);\
-				int16_t img_one_block = __getOneBlock__(img_data,temp_img_rem);\
-				dest2_ind = (n1+2*pad+pad)+i+j*(n1+2*pad)+(k*(n1+2*pad)*(n2+2*pad));\
-				uint8_t temp_out_rem = dest2_ind - 4*(dest2_ind >> 2);\
-				dest2.data_array[dest2_ind >> 2] = __putOneBlock__(dest2.data_array[dest2_ind >> 2],temp_out_rem,img_one_block);\
+				for (k = 0;k < n3;k = k + 3)\
+				{\
+					int img_index[3] = {i,j,k};\
+					int dest_index[3] = {i+pad,j+pad,k};\
+					int img_data_array_idx = __GetTensorEntryIndexOffset__(T,img_index);\
+					int dest_data_array_idx = __GetTensorEntryIndexOffset__(R,dest_index);\
+					uint64_t img_data = T.data_array[img_data_array_idx >> 2];\
+					uint8_t temp_img_rem = img_data_array_idx - 4*(img_data_array_idx >> 2);\
+					int16_t img_one_block = __getOneBlock__(img_data,temp_img_rem);\
+					uint8_t temp_out_rem = dest_data_array_idx - 4*(dest_data_array_idx >> 2);\
+					R.data_array[dest_data_array_idx >> 2] = __putOneBlock__(R.data_array[dest_data_array_idx >> 2],temp_out_rem,img_one_block);\
+					int img_index1[3] = {i,j,k+1};\
+					int dest_index1[3] = {i+pad,j+pad,k+1};\
+					int img_data_array_idx1 = __GetTensorEntryIndexOffset__(T,img_index1);\
+					int dest_data_array_idx1 = __GetTensorEntryIndexOffset__(R,dest_index1);\
+					uint64_t img_data1 = T.data_array[img_data_array_idx1 >> 2];\
+					uint8_t temp_img_rem1 = img_data_array_idx1 - 4*(img_data_array_idx1 >> 2);\
+					int16_t img_one_block1 = __getOneBlock__(img_data1,temp_img_rem1);\
+					uint8_t temp_out_rem1 = dest_data_array_idx1 - 4*(dest_data_array_idx1 >> 2);\
+					R.data_array[dest_data_array_idx1 >> 2] = __putOneBlock__(R.data_array[dest_data_array_idx1 >> 2],temp_out_rem1,img_one_block1);\
+					int img_index2[3] = {i,j,k+2};\
+					int dest_index2[3] = {i+pad,j+pad,k+2};\
+					int img_data_array_idx2 = __GetTensorEntryIndexOffset__(T,img_index2);\
+					int dest_data_array_idx2 = __GetTensorEntryIndexOffset__(R,dest_index2);\
+					uint64_t img_data2 = T.data_array[img_data_array_idx2 >> 2];\
+					uint8_t temp_img_rem2 = img_data_array_idx2 - 4*(img_data_array_idx2 >> 2);\
+					int16_t img_one_block2 = __getOneBlock__(img_data2,temp_img_rem2);\
+					uint8_t temp_out_rem2 = dest_data_array_idx2 - 4*(dest_data_array_idx2 >> 2);\
+					R.data_array[dest_data_array_idx2 >> 2] = __putOneBlock__(R.data_array[dest_data_array_idx2 >> 2],temp_out_rem2,img_one_block2);\
+				}\
+				__loop_pipeline_var__;\
 			}\
-		}\
-	}\
-    int index=0;\
-    int jump_matrix = ((n1+2*pad)*(n2+2*pad));\
-    int row_jump = (n1+2*pad);\
-    int column_jump = (n2+2*pad);\
-	out_idx = -1;\
-    for(i=0;i<n1+2*pad;i++)\
-	{\
-		out_idx++;\
-		result_temp = 0;\
-		for(j=0;j<n2+2*pad;j++)\
-		{\
-			for(k=0;k<n3;k++)\
-			{\
-				dest2_ind = k*jump_matrix +  j*(n1+2*pad) + i ;\
-                uint64_t img_data = dest2.data_array[dest2_ind >> 2];\
-				uint8_t temp_img_rem = dest2_ind - 4*(dest2_ind >> 2);\
-				int16_t img_one_block = __getOneBlock__(img_data,temp_img_rem);\
-				uint8_t temp_out_rem = index - 4*(index >> 2);\
-				R.data_array[index >> 2] = __putOneBlock__(R.data_array[index >> 2],temp_out_rem,img_one_block);\
-				index = index + 1 ;\
-			}\
-		}\
 	}\
 })
 #define __getOneBlock__(array64bit,position)   ({\

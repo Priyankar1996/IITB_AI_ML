6f8c332d2553788411ed3427d9637ad41b315476
deval1812 <30732768+deval1812@users.noreply.github.com>
2022-03-01 20:55:40 +0530

    Add files via upload

--- C/hw_modules/convolution/hw/src/convolveTensors.h
+++ C/hw_modules/convolution/hw/src/convolveTensors.h
@@ -4,9 +4,18 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <float.h>
+#include <Pipes.h>
+#include "pipeHandler.h"
 #include "sized_tensor.h"
 
 void conv2D();
+
+#ifndef SW
+void __loop_pipelining_on__(uint32_t pipeline_depth, uint32_t buffering, uint32_t full_rate);
+	#define __loop_pipeline_var__ __loop_pipelining_on__(15,1,1);
+#else
+	#define __loop_pipeline_var__ {;}
+#endif
 
 #define __dim0__(T) ({T.descriptor.descriptor.dimensions[0];})
 #define __dim1__(T) ({T.descriptor.descriptor.dimensions[1];})
@@ -21,44 +30,58 @@ void conv2D();
 	out.descriptor.descriptor.dimensions[1] = 1 + __udiv16__(__dim1__(inp) - __dim2__(ker),stride);\
 	out.descriptor.descriptor.dimensions[2] = __dim0__(ker);\
 	out.descriptor.tensor_size = __dim0__(out) * __dim1__(out) * __dim2__(out);\
-	int out_idx = -1;\
-	int16_t result_temp;\
+	int16_t result_temp,temp;\
 	int p,q,r,i,j,k;\
+	int count = 0;\
+	uint64_t img_data;\
+	uint64_t ker_data;\
 	for (p = 0; p < __dim0__(out); p++)\
 	{\
 		for(q = 0; q < __dim1__(out); q++)\
 		{\
 			for(r = 0; r < __dim2__(out); r++)\
 			{\
-				out_idx++;\
 				result_temp = 0;\
-				for(i = 0; i < __dim1__(ker); i++)\
+				while(i < __dim1__(ker))\
 				{\
-					for(j = 0; j < __dim2__(ker); j++)\
+					int img_data_array_idx = __GetTensorEntryIndexOffset__(inp,0,p*stride+i,q*stride+j,k);\
+					int ker_data_array_idx = __GetTensorEntryIndexOffset__(ker,r,i,j,k);\
+					uint64_t img_data = inp.data_array[img_data_array_idx >> 2];\
+					uint64_t ker_data = ker.data_array[ker_data_array_idx >> 2];\
+					if((count - 4*(count >> 2)) == 0)\
 					{\
-						for(k = 0; k < __dim3__(ker); k++)\
+						__getOneTensorBlock__(inp,img_data,img_data_array_idx);\
+						__getOneTensorBlock__(ker,ker_data,ker_data_array_idx);\
+					}\
+					uint8_t temp_img_rem = img_data_array_idx - 4*(img_data_array_idx >> 2);\
+					uint8_t temp_ker_rem = ker_data_array_idx - 4*(ker_data_array_idx >> 2);\
+					int16_t img_one_block = __getOneBlock__(img_data,temp_img_rem);\
+					int16_t ker_one_block = __getOneBlock__(ker_data,temp_ker_rem);\
+					temp = result_temp;\
+					result_temp = temp + (img_one_block * ker_one_block);\
+					k++;\
+					if(k == __dim3__(ker))\
+					{\
+						k = 0;\
+						j++;\
+						if(j == __dim2__(ker))\
 						{\
-							int img_index[3] = {p*stride+i,q*stride+j,k};\
-							int ker_index[4] = {r,i,j,k};\
-							int img_data_array_idx = __GetTensorEntryIndexOffset__(inp,img_index);\
-							int ker_data_array_idx = __GetTensorEntryIndexOffset__(ker,ker_index);\
-							uint64_t img_data = inp.data_array[img_data_array_idx >> 2];\
-							uint64_t ker_data = ker.data_array[ker_data_array_idx >> 2];\
-							uint8_t temp_img_rem = img_data_array_idx - 4*(img_data_array_idx >> 2);\
-							uint8_t temp_ker_rem = ker_data_array_idx - 4*(ker_data_array_idx >> 2);\
-							int16_t img_one_block = __getOneBlock__(img_data,temp_img_rem);\
-							int16_t ker_one_block = __getOneBlock__(ker_data,temp_ker_rem);\
-							result_temp += img_one_block * ker_one_block;\
+							j = 0;\
+							i++;\
 						}\
 					}\
+					count++;\
 				}\
-				int out_index[3] = {p,q,r};\
-				int out_data_array_idx = __GetTensorEntryIndexOffset__(out,out_index);\
+				int out_data_array_idx = __GetTensorEntryIndexOffset__(out,0,p,q,r);\
 				uint8_t temp_out_rem = out_data_array_idx - 4*(out_data_array_idx >> 2);\
-				out.data_array[out_data_array_idx >> 2] = __putOneBlock__(out.data_array[out_data_array_idx >> 2],temp_out_rem,result_temp);\
+				__putOneBlock__(out.data_array[out_data_array_idx >> 2],temp_out_rem,result_temp);\
 			}\
 		}\
 	}\
+})
+
+#define __getOneTensorBlock__(tnsr,data,index)({\
+	data = tnsr.data_array[index >> 2];\
 })
 
 #define __udiv16__(dividend,divisor)({\
@@ -97,5 +120,4 @@ void conv2D();
 
 #define __putOneBlock__(array64bit,position,result)({\
 	array64bit = array64bit | (((uint64_t)result) << (position*16));\
-	array64bit;\
 })

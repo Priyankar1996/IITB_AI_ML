f1e1cf28f6696f1c38971f9471d97db942861490
= <=>
2022-03-02 12:26:28 +0530

    Update maxPool

--- C/hw_modules/maxPool/hw_4_l2/src/maxPoolOfTensors.h
+++ C/hw_modules/maxPool/hw_4_l2/src/maxPoolOfTensors.h
@@ -8,6 +8,10 @@
 #include "sized_tensor.h"
 
 void maxPool3D();
+void maxPoolCore1();
+void maxPoolCore2();
+void maxPoolCore3();
+void maxPoolCore4();
 
 #define __I16 1
 #define __dim0__(A) ({A.descriptor.descriptor.dimensions[0];})
@@ -21,8 +25,10 @@ void maxPool3D();
 #ifndef SW
 void __loop_pipelining_on__(uint32_t pipeline_depth, uint32_t buffering, uint32_t full_rate);
 	#define __loop_pipeline_var__ __loop_pipelining_on__(15,1,1);
+void __aa_barrier__();
 #else
 	#define __loop_pipeline_var__ {;}
+	#define __aa_barrier__() {;}
 #endif
 
 #define __increment__(row,col,chl,max_col,max_chl) ({\
@@ -33,6 +39,18 @@ void __loop_pipelining_on__(uint32_t pipeline_depth, uint32_t buffering, uint32_
 	}\
 	if (col == max_col){\
 		col = 0;\
+		row++;\
+	}\
+})
+
+#define __increment_mm__(row,col,chl,min_col,max_col,max_chl) ({\
+	chl++;\
+	if (chl == max_chl){\
+		chl = 0;\
+		col++;\
+	}\
+	if (col == max_col){\
+		col = min_col;\
 		row++;\
 	}\
 })
@@ -76,28 +94,46 @@ void __loop_pipelining_on__(uint32_t pipeline_depth, uint32_t buffering, uint32_
 		if (min_val2 > min_val6) min_val10 = min_val2; else min_val10 = min_val6;\
 		if (min_val3 > min_val7) min_val11 = min_val3; else min_val11 = min_val7;\
 		if (min_val4 > min_val8) min_val12 = min_val4; else min_val12 = min_val8;\
-		int64_t element = min_val12;\
-		element <<= 16;\
-		element += min_val11;\
-		element <<= 16;\
-		element += min_val10;\
-		element <<= 16;\
-		element += min_val9;\
+		int64_t element1 = min_val12;\
+		int64_t element2 = min_val11;\
+		int64_t element3 = min_val10;\
+		int64_t element4 = min_val9;\
+		element1 <<= 48;\
+		element2 <<= 32;\
+		element3 <<= 16;\
+		int64_t element = element1 + element2 + element3 + element4;\
 		element;\
 	})
 
 #define __maxPoolOfTensors3D__(src, dst, stride) ({\
 	uint32_t address = 0, add_src;\
-	uint32_t offset1 = __dim22__(src), offset2 = __dim1__(src)*offset1;\
 	uint16_t dim1 = __dim1__(dst), dim1d = __dim1__(src);\
+	uint32_t offset1 = __dim22__(src), offset2 = dim1d*offset1;\
 	uint16_t row=0,col=0,chl=0;\
-	uint32_t size = __NumberOfElementsInSizedTensor__(dst);\
+	uint32_t dim0 = __dim0__(dst);\
 	while(1)\
 	{\
-		add_src = chl+offset1*(col*stride+dim1d*row*stride);\
+		__loop_pipeline_var__\
+		add_src = chl+offset1*stride*(col+dim1d*row);\
 		dst.data_array[address] = __maxOperation4__(src,add_src,offset1,offset2);\
 		address++;\
 		__increment__(row,col,chl,dim1,offset1);\
-		if (address == (size>>2)) break;\
+		if (row == dim0) break;\
+	}\
+})
+
+#define __maxPoolOfTensors3D_div__(src, dst, stride, rs, cs, re, ce) ({\
+	uint32_t address, add_src;\
+	uint16_t dim1d = __dim1__(src), dim1 = __dim1__(dst);\
+	uint32_t offset1 = __dim22__(dst), offset2 = dim1d*offset1;\
+	uint16_t row=rs,col=cs,chl=0;\
+	while(1)\
+	{\
+		__loop_pipeline_var__\
+		address = chl+offset1*(col+dim1*row);\
+		add_src = chl+offset1*stride*(col+dim1d*row);\
+		dst.data_array[address] = __maxOperation4__(src,add_src,offset1,offset2);\
+		__increment_mm__(row,col,chl,cs,ce,offset1);\
+		if (row == re) break;\
 	}\
 })

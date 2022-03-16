c6f5ad377f7da353b023a0f6005c1f2d4d87cc87
deval1812 <30732768+deval1812@users.noreply.github.com>
2022-03-16 17:10:18 +0530

    Update

--- C/hw_modules/convolution/hw/src/prog.c
+++ C/hw_modules/convolution/hw/src/prog.c
@@ -8,36 +8,41 @@
 #include "sized_tensor.h"
 #include "convolveTensors.h"
 
-//#ifndef SW
-//void __loop_pipelining_on__(uint32_t pipeline_depth, uint32_t buffering, uint32_t full_rate);
-//#endif
+#ifndef SW
+void __loop_pipelining_on__(uint32_t pipeline_depth, uint32_t buffering, uint32_t full_rate);
+	#define __loop_pipeline_var__ __loop_pipelining_on__(15,1,1);
+void __aa_barrier__();
+#else
+	#define __loop_pipeline_var__ {;}
+	#define __aa_barrier__() {;}
+#endif
 
 SizedTensor_16K T,K,R;
-//SizedTensor_1024 K;
+TensorDescriptor desc_T,desc_K,desc_R;
 uint8_t stride;
 
 void getInput()
 {
 	int i;
-	T.descriptor.descriptor.data_type = i16;
-	K.descriptor.descriptor.data_type = i16;
-	T.descriptor.descriptor.row_major_form = 1;
-	K.descriptor.descriptor.row_major_form = 1;
-	T.descriptor.descriptor.number_of_dimensions = 3;
-	K.descriptor.descriptor.number_of_dimensions = 4;
-	for(i = 0; i < T.descriptor.descriptor.number_of_dimensions; i++)
+	desc_T.data_type = i16;
+	desc_K.data_type = i16;
+	desc_T.row_major_form = 1;
+	desc_K.row_major_form = 1;
+	desc_T.number_of_dimensions = 3;
+	desc_K.number_of_dimensions = 4;
+	for(i = 0; i < desc_T.number_of_dimensions; i++)
 	{
-		T.descriptor.descriptor.dimensions[i] = read_uint64("conv_input_pipe");
+		desc_T.dimensions[i] = read_uint64("conv_input_pipe");
 	}
-	for(i = 0; i < (__NumberOfElementsInSizedTensor__(T) >> 2)+1; i++)
+	for(i = 0; i < (__NumberOfElementsInSizedTensor__(desc_T) >> 2)+1; i++)
 	{
 		T.data_array[i] = read_uint64("conv_input_pipe");
 	}
-	for(i = 0; i < K.descriptor.descriptor.number_of_dimensions; i++)
+	for(i = 0; i < desc_K.number_of_dimensions; i++)
 	{
-		K.descriptor.descriptor.dimensions[i] = read_uint64("conv_input_pipe");
+		desc_K.dimensions[i] = read_uint64("conv_input_pipe");
 	}
-	for(i = 0; i < (__NumberOfElementsInSizedTensor__(K) >> 2)+1; i++)
+	for(i = 0; i < (__NumberOfElementsInSizedTensor__(desc_K) >> 2)+1; i++)
 	{
 		K.data_array[i] = read_uint64("conv_input_pipe");
 	}
@@ -47,30 +52,146 @@ void getInput()
 void sendOutput()
 {
 	int i;
-	R.descriptor.descriptor.data_type = i16;
-	R.descriptor.descriptor.row_major_form = 1;
-	R.descriptor.descriptor.number_of_dimensions = 3;
-	for(i = 0; i < R.descriptor.descriptor.number_of_dimensions; i++)
+	desc_R.data_type = i16;\
+    	desc_R.number_of_dimensions = 3;\
+    	desc_R.row_major_form = 1;\
+	for(i = 0; i < desc_R.number_of_dimensions; i++)
 	{
-		write_uint64("conv_output_pipe",R.descriptor.descriptor.dimensions[i]);
+		write_uint64("conv_output_pipe",desc_R.dimensions[i]);
 	}
-	for(i = 0; i < (__NumberOfElementsInSizedTensor__(R) >> 2)+1; i++)
+	for(i = 0; i < (__NumberOfElementsInSizedTensor__(desc_R) >> 2)+1; i++)
 	{
 		write_uint64("conv_output_pipe",R.data_array[i]);
 	}
 }
 
+void convCore1()
+{
+	uint16_t p_start = read_uint16("core1_req_pipe");
+	uint16_t q_start = read_uint16("core1_req_pipe");
+	uint16_t r_start = read_uint16("core1_req_pipe");
+	uint16_t p_end = read_uint16("core1_req_pipe");
+	uint16_t q_end = read_uint16("core1_req_pipe");
+	uint16_t r_end = read_uint16("core1_req_pipe");
+	__aa_barrier__();	
+	__convolveTensors__(T,K,R,stride,desc_T,desc_K,desc_R,p_start,q_start,r_start,p_end,q_end,r_end);
+	__aa_barrier__();
+	write_uint16("core1_ack_pipe",1);
+}
+
+void convCore2()
+{
+	uint16_t p_start = read_uint16("core2_req_pipe");
+	uint16_t q_start = read_uint16("core2_req_pipe");
+	uint16_t r_start = read_uint16("core2_req_pipe");
+	uint16_t p_end = read_uint16("core2_req_pipe");
+	uint16_t q_end = read_uint16("core2_req_pipe");
+	uint16_t r_end = read_uint16("core2_req_pipe");
+	__aa_barrier__();	
+	__convolveTensors__(T,K,R,stride,desc_T,desc_K,desc_R,p_start,q_start,r_start,p_end,q_end,r_end);
+	__aa_barrier__();
+	write_uint16("core2_ack_pipe",1);
+}
+
+void convCore3()
+{
+	uint16_t p_start = read_uint16("core3_req_pipe");
+	uint16_t q_start = read_uint16("core3_req_pipe");
+	uint16_t r_start = read_uint16("core3_req_pipe");
+	uint16_t p_end = read_uint16("core3_req_pipe");
+	uint16_t q_end = read_uint16("core3_req_pipe");
+	uint16_t r_end = read_uint16("core3_req_pipe");
+	__aa_barrier__();	
+	__convolveTensors__(T,K,R,stride,desc_T,desc_K,desc_R,p_start,q_start,r_start,p_end,q_end,r_end);
+	__aa_barrier__();
+	write_uint16("core3_ack_pipe",1);
+}
+
+void convCore4()
+{
+	uint16_t p_start = read_uint16("core4_req_pipe");
+	uint16_t q_start = read_uint16("core4_req_pipe");
+	uint16_t r_start = read_uint16("core4_req_pipe");
+	uint16_t p_end = read_uint16("core4_req_pipe");
+	uint16_t q_end = read_uint16("core4_req_pipe");
+	uint16_t r_end = read_uint16("core4_req_pipe");
+	__aa_barrier__();	
+	__convolveTensors__(T,K,R,stride,desc_T,desc_K,desc_R,p_start,q_start,r_start,p_end,q_end,r_end);
+	__aa_barrier__();
+	write_uint16("core4_ack_pipe",1);
+}
+
 void conv2D()
 {
     getInput();
-    #ifndef SW
-	    uint64_t start_time = timer();
-    #endif
-    __convolveTensors__(T,K,R,stride);
-    #ifndef SW
-	    uint64_t stop_time = timer();
-	    uint64_t elapsed_time = stop_time - start_time;
-	    write_uint64("elapsed_time_pipe", elapsed_time);
-    #endif
+	__aa_barrier__();
+	uint16_t p_start1 = 0;
+	uint16_t q_start1 = 0;
+	uint16_t r_start1 = 0;
+	uint16_t p_end1 = 0;
+	uint16_t q_end1 = 3;
+	uint16_t r_end1 = 0;
+
+	uint16_t p_start2 = 1;
+	uint16_t q_start2 = 0;
+	uint16_t r_start2 = 0;
+	uint16_t p_end2 = 1;
+	uint16_t q_end2 = 3;
+	uint16_t r_end2 = 0;
+	
+	uint16_t p_start3 = 2;
+	uint16_t q_start3 = 0;
+	uint16_t r_start3 = 0;
+	uint16_t p_end3 = 2;
+	uint16_t q_end3 = 3;
+	uint16_t r_end3 = 0;
+	
+	uint16_t p_start4 = 3;
+	uint16_t q_start4 = 0;
+	uint16_t r_start4 = 0;
+	uint16_t p_end4 = 3;
+	uint16_t q_end4 = 3;
+	uint16_t r_end4 = 0;
+	__aa_barrier__();
+#ifndef SW
+	uint64_t start_time = timer();
+#endif
+	write_uint16("core1_req_pipe",p_start1);
+	write_uint16("core1_req_pipe",q_start1);
+	write_uint16("core1_req_pipe",r_start1);
+	write_uint16("core1_req_pipe",p_end1);
+	write_uint16("core1_req_pipe",q_end1);
+	write_uint16("core1_req_pipe",r_end1);
+	write_uint16("core2_req_pipe",p_start2);
+	write_uint16("core2_req_pipe",q_start2);
+	write_uint16("core2_req_pipe",r_start2);
+	write_uint16("core2_req_pipe",p_end2);
+	write_uint16("core2_req_pipe",q_end2);
+	write_uint16("core2_req_pipe",r_end2);
+	write_uint16("core3_req_pipe",p_start3);
+	write_uint16("core3_req_pipe",q_start3);
+	write_uint16("core3_req_pipe",r_start3);
+	write_uint16("core3_req_pipe",p_end3);
+	write_uint16("core3_req_pipe",q_end3);
+	write_uint16("core3_req_pipe",r_end3);
+	write_uint16("core4_req_pipe",p_start4);
+	write_uint16("core4_req_pipe",q_start4);
+	write_uint16("core4_req_pipe",r_start4);
+	write_uint16("core4_req_pipe",p_end4);
+	write_uint16("core4_req_pipe",q_end4);
+	write_uint16("core4_req_pipe",r_end4);
+	
+	__aa_barrier__();
+	uint8_t done1 = read_uint16("core1_ack_pipe");
+	uint8_t done2 = read_uint16("core2_ack_pipe");
+	uint8_t done3 = read_uint16("core3_ack_pipe");
+	uint8_t done4 = read_uint16("core4_ack_pipe");
+	__aa_barrier__();
+#ifndef SW
+	uint64_t stop_time = timer();
+	uint64_t elapsed_time = stop_time - start_time;
+	write_uint64("elapsed_time_pipe", elapsed_time);
+#endif
+	__aa_barrier__();
     sendOutput();
 }

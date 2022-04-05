fcfbbff0f35f1fe3717f22ea8a506e40a0125e53
AjinkyaRaghuwanshi <ajinkyaraghuwanshi@gmail.com>
2022-04-05 16:50:02 +0530

    Tried FPGA implementation, but there are errors. The errors are mentioned in Error_log.txt

---
+++ fpga/zeropad/misc_vhdl/Aa/vhdl/RtUart/baud_control_calculator_global_package.vhdl
@@ -0,0 +1,12 @@
+-- VHDL global package produced by vc2vhdl from virtual circuit (vc) description 
+library ieee;
+use ieee.std_logic_1164.all;
+package baud_control_calculator_global_package is -- 
+  component baud_control_calculator is -- 
+    port (-- 
+      clk : in std_logic;
+      reset : in std_logic); -- 
+    -- 
+  end component;
+  -- 
+end package baud_control_calculator_global_package;

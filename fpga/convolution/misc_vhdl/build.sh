fed79c55ae3974ffd01245b0028c1bb5bcb388e6
Deval Biren Patel <dbpatel96@gmail.com>
2022-04-06 16:30:02 +0530

    Update

---
+++ fpga/convolution/misc_vhdl/build.sh
@@ -0,0 +1,10 @@
+CWD=$(pwd)
+cd Aa
+make
+cd $CWD
+cat ./package.vhdl > lib/RtUart.vhdl
+cat ./rt_clock_counter.vhdl >> lib/RtUart.vhdl
+cat ./Aa/vhdl/RtUart/baud_control_calculator_global_package.vhdl >> lib/RtUart.vhdl
+cat ./Aa/vhdl/RtUart/baud_control_calculator_test_bench.vhdl >> lib/RtUart.vhdl
+cat ./Aa/vhdl/RtUart/baud_control_calculator.vhdl >> lib/RtUart.vhdl
+cat ./configurable_self_tuning_uart.vhdl >> lib/RtUart.vhdl
---
+++ fpga/convolution/vivado_synth/run.sh
@@ -0,0 +1,3 @@
+# source /vivado/Vivado/2014.4/settings64.sh
+source /home/deval/work/Vivado/Vivado/2017.4/settings64.sh
+vivado -mode batch -source bitgen.tcl

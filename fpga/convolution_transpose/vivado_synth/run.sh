2d9aada7e6eba35f4ffc256ce0903e235e249ca1
Priyankar Sarkar <priyansrk@gmail.com>
2022-04-11 15:22:34 +0530

    Updated

--- fpga/convolution_transpose/vivado_synth/run.sh
+++ fpga/convolution_transpose/vivado_synth/run.sh
@@ -1,3 +1,3 @@
 # source /vivado/Vivado/2014.4/settings64.sh
-source /home/priyankar/SDK/2017.4/settings64.sh
+source /home/AIML/my_xil_dir/Vivado/2017.1/settings64.sh
 vivado -mode batch -source bitgen.tcl

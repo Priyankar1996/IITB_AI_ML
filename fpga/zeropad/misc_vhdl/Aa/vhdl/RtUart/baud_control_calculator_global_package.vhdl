5fdb04c63a26c731a91021ac7e2a0e7d7bc3b2ef
Deval Biren Patel <dbpatel96@gmail.com>
2022-04-20 15:23:44 +0530

    Update

--- fpga/zeropad/misc_vhdl/Aa/vhdl/RtUart/baud_control_calculator_global_package.vhdl
+++ fpga/zeropad/misc_vhdl/Aa/vhdl/RtUart/baud_control_calculator_global_package.vhdl
@@ -5,7 +5,12 @@ package baud_control_calculator_global_package is --
   component baud_control_calculator is -- 
     port (-- 
       clk : in std_logic;
-      reset : in std_logic); -- 
+      reset : in std_logic;
+      BAUD_CONTROL_WORD_SIG: out std_logic_vector(31 downto 0);
+      BAUD_CONTROL_WORD_VALID: out std_logic_vector(0 downto 0);
+      BAUD_RATE_SIG: in std_logic_vector(31 downto 0);
+      CLK_FREQUENCY_SIG: in std_logic_vector(31 downto 0);
+      CLOCK_FREQUENCY_VALID: in std_logic_vector(0 downto 0)); -- 
     -- 
   end component;
   -- 
---
+++ fpga/zeropad/misc_vhdl/Aa/vhdlCStubs.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <Pipes.h>
+#include <SocketLib.h>
+void baudControlCalculatorDaemon();
+void global_storage_initializer_();
+uint32_t my_div(uint32_t A,uint32_t B);
+uint32_t my_gcd(uint32_t A,uint32_t B);

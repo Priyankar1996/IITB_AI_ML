e4b0f65e5d076e5f08f3ebd92fabc64d8fe52433
AjinkyaRaghuwanshi <ajinkyaraghuwanshi@gmail.com>
2022-04-20 17:23:10 +0530

    Correcting the pre running hw model of C

--- C/hw_modules/zeropad/hw/src/prog.c
+++ C/hw_modules/zeropad/hw/src/prog.c
@@ -11,7 +11,7 @@
 
 SizedTensor_16K T,R;
 TensorDescriptor des_inp,des_out;
-uint8_t pad;
+uint16_t pad;
 
 #ifndef SW
 void __loop_pipelining_on__(uint32_t pipeline_depth, uint32_t buffering, uint32_t full_rate);
@@ -25,42 +25,26 @@ void __aa_barrier__();
 #define __dt__ int16_t
 
 #define __get4xi16__(element) ({\
-	element = read_uint8 ("zeropad_input_pipe");\
-	element = (element << 16) + read_uint8 ("zeropad_input_pipe");\
-	element = (element << 16) + read_uint8 ("zeropad_input_pipe");\
-	element = (element << 16) + read_uint8 ("zeropad_input_pipe");\
-	element = (element << 16) + read_uint8 ("zeropad_input_pipe");\
-	element = (element << 16) + read_uint8 ("zeropad_input_pipe");\
-	element = (element << 16) + read_uint8 ("zeropad_input_pipe");\
-	element = (element << 16) + read_uint8 ("zeropad_input_pipe");\
+	element = read_uint16 ("zeropad_input_pipe");\
+	element = (element << 16) + read_uint16 ("zeropad_input_pipe");\
+	element = (element << 16) + read_uint16 ("zeropad_input_pipe");\
+	element = (element << 16) + read_uint16 ("zeropad_input_pipe");\
 })
 
 #define __set4xi16__(addr) ({\
 	uint64_t element = R.data_array[addr];\
-	uint8_t out_data[8];\
-	out_data[7] = element & 0xFF;\
-	element>>=8;\
-	out_data[6] = element & 0xFF;\
-	element>>=8;\
-	out_data[5]= element & 0xFF;\
-	element>>=8;\
-    out_data[4] = element & 0xFF;\
-	element>>=8;\
-	out_data[3] = element & 0xFF;\
-	element>>=8;\
-	out_data[2] = element & 0xFF;\
-	element>>=8;\
-    out_data[1] = element & 0xFF;\
-	element>>=8;\
-	out_data[0] = element & 0xFFFF;\
-	write_uint8 ("zeropad_output_pipe",out_data[0]);\
-	write_uint8 ("zeropad_output_pipe",out_data[1]);\
-	write_uint8 ("zeropad_output_pipe",out_data[2]);\
-	write_uint8 ("zeropad_output_pipe",out_data[3]);\
-	write_uint8 ("zeropad_output_pipe",out_data[4]);\
-	write_uint8 ("zeropad_output_pipe",out_data[5]);\
-	write_uint8 ("zeropad_output_pipe",out_data[6]);\
-	write_uint8 ("zeropad_output_pipe",out_data[7]);\
+	__dt__ out_data[4];\
+	out_data[3] = element & 0xFFFF;\
+	element>>=16;\
+	out_data[2] = element & 0xFFFF;\
+	element>>=16;\
+	out_data[1]= element & 0xFFFF;\
+	element>>=16;\
+    out_data[0] = element & 0xFFFF;\
+	write_uint16 ("zeropad_output_pipe",out_data[0]);\
+	write_uint16 ("zeropad_output_pipe",out_data[1]);\
+	write_uint16 ("zeropad_output_pipe",out_data[2]);\
+	write_uint16 ("zeropad_output_pipe",out_data[3]);\
 })
 
 uint64_t getRemainingElements(uint16_t ne){
@@ -89,18 +73,18 @@ void sendRemainingElements(int addr, uint16_t ne){
 uint16_t testConfigure()
 {
     des_inp.data_type = i16;
-    des_inp.row_major_form = read_uint8 ("zeropad_input_pipe");;
-    des_inp.number_of_dimensions = read_uint8 ("zeropad_input_pipe");
+    des_inp.row_major_form = read_uint16 ("zeropad_input_pipe");;
+    des_inp.number_of_dimensions = read_uint16 ("zeropad_input_pipe");
     int i;
     for(i = 0;i < des_inp.number_of_dimensions;i++){
-        des_inp.dimensions[i] = read_uint8 ("zeropad_input_pipe");
+        des_inp.dimensions[i] = read_uint16 ("zeropad_input_pipe");
     }
 
-    pad = read_uint8 ("zeropad_input_pipe");
+    pad = read_uint16 ("zeropad_input_pipe");
     
-	des_out.dimensions[0] = read_uint8 ("zeropad_input_pipe");
-    des_out.dimensions[1] = read_uint8 ("zeropad_input_pipe");
-    des_out.dimensions[2] = read_uint8 ("zeropad_input_pipe");
+	des_out.dimensions[0] = read_uint16 ("zeropad_input_pipe");
+    des_out.dimensions[1] = read_uint16 ("zeropad_input_pipe");
+    des_out.dimensions[2] = read_uint16 ("zeropad_input_pipe");
     
 	// uint64_t input_size = __NumberOfElementsInSizedTensor__(T);
     uint64_t input_size = des_inp.dimensions[0]*des_inp.dimensions[1]*des_inp.dimensions[2];
@@ -282,9 +266,9 @@ void zeropad3D()
     uint16_t rv = testConfigure();
     __aa_barrier__();
 
-    // #ifndef SW
-	//     uint64_t start_time = timer();
-    // #endif
+    #ifndef SW
+	    uint64_t start_time = timer();
+    #endif
     write_uint16("Block0_starting", rv);
     write_uint16("Block1_starting", rv);
     write_uint16("Block2_starting", rv);
@@ -303,12 +287,12 @@ void zeropad3D()
     uint16_t s7 = read_uint16("Block7_complete");   
     __aa_barrier__();
     
-    // #ifndef SW
-	//     uint64_t stop_time = timer();
-	//     uint64_t elapsed_time = stop_time - start_time;
-	//     write_uint64("elapsed_time_pipe", elapsed_time);
-    // #endif
-    // __aa_barrier__();
+    #ifndef SW
+	    uint64_t stop_time = timer();
+	    uint64_t elapsed_time = stop_time - start_time;
+	    write_uint64("elapsed_time_pipe", elapsed_time);
+    #endif
+    __aa_barrier__();
 
     sendOutput();
 }

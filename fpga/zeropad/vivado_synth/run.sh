70a41fa96d281c33883bc6c0695e281e88c92164
Deval Biren Patel <dbpatel96@gmail.com>
2022-04-20 15:23:44 +0530

    Update

--- fpga/zeropad/vivado_synth/run.sh
+++ fpga/zeropad/vivado_synth/run.sh
@@ -1,3 +1,2 @@
-# source /vivado/Vivado/2014.4/settings64.sh
-source /opt/Xilinx/Vivado/2017.2/settings64.sh
+source /home/deval/work/Vivado/Vivado/2017.4/settings64.sh
 vivado -mode batch -source bitgen.tcl

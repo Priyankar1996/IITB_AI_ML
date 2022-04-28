ed1d58a7d3e68c844e2fa158309c44dadebd5790
AjinkyaRaghuwanshi <ajinkyaraghuwanshi@gmail.com>
2022-04-28 16:53:19 +0530

    Correction in buils

--- students/Ajinkya_Zero_padding/hw/zeropad_hw/zeropad_fpga_1/misc_vhdl/build.sh
+++ students/Ajinkya_Zero_padding/hw/zeropad_hw/zeropad_fpga_1/misc_vhdl/build.sh
@@ -5,6 +5,5 @@ cd $CWD
 cat ./package.vhdl > lib/RtUart.vhdl
 cat ./rt_clock_counter.vhdl >> lib/RtUart.vhdl
 cat ./Aa/vhdl/RtUart/baud_control_calculator_global_package.vhdl >> lib/RtUart.vhdl
-cat ./Aa/vhdl/RtUart/baud_control_calculator_test_bench.vhdl >> lib/RtUart.vhdl
 cat ./Aa/vhdl/RtUart/baud_control_calculator.vhdl >> lib/RtUart.vhdl
 cat ./configurable_self_tuning_uart.vhdl >> lib/RtUart.vhdl

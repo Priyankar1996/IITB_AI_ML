5d3b993eacb94506eb6f84f125aeb5c4a837c8c1
AjinkyaRaghuwanshi <ajinkyaraghuwanshi@gmail.com>
2022-04-28 17:25:08 +0530

    Some changes

--- fpga/zeropad/vivado_synth/run.sh
+++ fpga/zeropad/vivado_synth/run.sh
@@ -1,2 +1,3 @@
-source /home/deval/work/Vivado/Vivado/2017.4/settings64.sh
+# source /home/Xilinx_2017.1/Vivado/2017.1/settings64.sh
+source /home/AIML/my_xil_dir/Vivado/2017.1/settings64.sh
 vivado -mode batch -source bitgen.tcl

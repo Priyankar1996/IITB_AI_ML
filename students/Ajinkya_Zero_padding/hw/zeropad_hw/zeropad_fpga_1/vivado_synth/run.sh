cd63966f42007d05911c5d4ffecbdc63fac65ee5
AjinkyaRaghuwanshi <ajinkyaraghuwanshi@gmail.com>
2022-04-28 16:18:16 +0530

    Changes in run.sh

--- students/Ajinkya_Zero_padding/hw/zeropad_hw/zeropad_fpga_1/vivado_synth/run.sh
+++ students/Ajinkya_Zero_padding/hw/zeropad_hw/zeropad_fpga_1/vivado_synth/run.sh
@@ -1,2 +1,3 @@
-source /home/Xilinx_2017.1/Vivado/2017.1/settings64.sh
+# source /home/Xilinx_2017.1/Vivado/2017.1/settings64.sh
+source /home/my_xil_dir/Vivado/2017.1/settings64.sh
 vivado -mode batch -source bitgen.tcl

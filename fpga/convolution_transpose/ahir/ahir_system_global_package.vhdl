ab9c671ab0e1444edc2cdfeae4ff7d948c9bb853
Priyankar Sarkar <priyansrk@gmail.com>
2022-05-03 16:32:04 +0530

    Updated

--- fpga/convolution_transpose/ahir/ahir_system_global_package.vhdl
+++ fpga/convolution_transpose/ahir/ahir_system_global_package.vhdl
@@ -2,9 +2,13 @@
 library ieee;
 use ieee.std_logic_1164.all;
 package ahir_system_global_package is -- 
-  constant count_base_address : std_logic_vector(0 downto 0) := "0";
+  constant desc_input_base_address : std_logic_vector(6 downto 0) := "0000000";
+  constant desc_kernel_base_address : std_logic_vector(6 downto 0) := "0000000";
+  constant desc_output_base_address : std_logic_vector(6 downto 0) := "0000000";
   constant input_base_address : std_logic_vector(13 downto 0) := "00000000000000";
-  constant kernel_base_address : std_logic_vector(11 downto 0) := "000000000000";
+  constant kernel_base_address : std_logic_vector(10 downto 0) := "00000000000";
   constant output_base_address : std_logic_vector(13 downto 0) := "00000000000000";
+  constant padding_base_address : std_logic_vector(0 downto 0) := "0";
+  constant stride_base_address : std_logic_vector(0 downto 0) := "0";
   -- 
 end package ahir_system_global_package;

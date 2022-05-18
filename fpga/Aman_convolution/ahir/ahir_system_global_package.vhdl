6b7f0fc608aee9ae6b4ffe6638f21d2eef3e675b
= <=>
2022-05-18 12:23:38 +0530

    Added ahir_system files - conv fpga

--- fpga/Aman_convolution/ahir/ahir_system_global_package.vhdl
+++ fpga/Aman_convolution/ahir/ahir_system_global_package.vhdl
@@ -2,7 +2,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 package ahir_system_global_package is -- 
-  constant B_base_address : std_logic_vector(13 downto 0) := "00000000000000";
+  constant K_base_address : std_logic_vector(13 downto 0) := "00000000000000";
   constant T_base_address : std_logic_vector(13 downto 0) := "00000000000000";
   constant count_base_address : std_logic_vector(0 downto 0) := "0";
   -- 

1b862a50f734f68b0884c8d8e46fc3f38b6dec6a
AjinkyaRaghuwanshi <ajinkyaraghuwanshi@gmail.com>
2022-05-18 20:17:29 +0530

    Changed the des_inp to a memory different

--- students/Ajinkya_Zero_padding/hw/zeropad_hw/fpga_zeropad_2/ahir/ahir_system_global_package.vhdl
+++ students/Ajinkya_Zero_padding/hw/zeropad_hw/fpga_zeropad_2/ahir/ahir_system_global_package.vhdl
@@ -4,9 +4,12 @@ use ieee.std_logic_1164.all;
 package ahir_system_global_package is -- 
   constant R_base_address : std_logic_vector(13 downto 0) := "00000000000000";
   constant T_base_address : std_logic_vector(13 downto 0) := "00000000000000";
+  constant col_high_base_address : std_logic_vector(0 downto 0) := "0";
   constant count_base_address : std_logic_vector(0 downto 0) := "0";
+  constant depth_high_base_address : std_logic_vector(0 downto 0) := "0";
   constant des_inp_base_address : std_logic_vector(6 downto 0) := "0000000";
   constant des_out_base_address : std_logic_vector(6 downto 0) := "0000000";
   constant pad_base_address : std_logic_vector(0 downto 0) := "0";
+  constant row_high_base_address : std_logic_vector(0 downto 0) := "0";
   -- 
 end package ahir_system_global_package;

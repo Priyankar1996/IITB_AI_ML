8b03d8d1c20d9f420474f2266244deb98602d3e1
AjinkyaRaghuwanshi <ajinkyaraghuwanshi@gmail.com>
2022-05-19 00:56:54 +0530

    Pushing the ahir_system.vhdl for the reduced memory prog.c

--- students/Ajinkya_Zero_padding/hw/zeropad_hw/fpga_zeropad_2/ahir/ahir_system_global_package.vhdl
+++ students/Ajinkya_Zero_padding/hw/zeropad_hw/fpga_zeropad_2/ahir/ahir_system_global_package.vhdl
@@ -4,14 +4,7 @@ use ieee.std_logic_1164.all;
 package ahir_system_global_package is -- 
   constant R_base_address : std_logic_vector(13 downto 0) := "00000000000000";
   constant T_base_address : std_logic_vector(13 downto 0) := "00000000000000";
-  constant col_high_base_address : std_logic_vector(0 downto 0) := "0";
   constant count_base_address : std_logic_vector(0 downto 0) := "0";
-  constant depth_high_base_address : std_logic_vector(0 downto 0) := "0";
-  constant des_inp_base_address : std_logic_vector(6 downto 0) := "0000000";
-  constant out_col_high_base_address : std_logic_vector(0 downto 0) := "0";
-  constant out_depth_high_base_address : std_logic_vector(0 downto 0) := "0";
-  constant out_row_high_base_address : std_logic_vector(0 downto 0) := "0";
   constant pad_base_address : std_logic_vector(0 downto 0) := "0";
-  constant row_high_base_address : std_logic_vector(0 downto 0) := "0";
   -- 
 end package ahir_system_global_package;

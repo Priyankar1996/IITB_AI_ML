79cb00365d9b9dc1ba486913afc29c3138aaa860
Priyankar Sarkar <priyansrk@gmail.com>
2022-05-19 13:38:20 +0530

    Updated

--- fpga/convolution_transpose/ahir/src/prog.c
+++ fpga/convolution_transpose/ahir/src/prog.c
@@ -65,96 +65,100 @@ void convTransposeA()
 {
     uint16_t i[3],k[4],o[3],s,p;
     uint32_t offset;
-    i[0]   = read_uint32("Block0_start");
-    i[1]   = read_uint32("Block0_start");
-    i[2]   = read_uint32("Block0_start");
-    k[0]   = read_uint32("Block0_start");
-    k[1]   = read_uint32("Block0_start");
-    k[2]   = read_uint32("Block0_start");
-    k[3]   = read_uint32("Block0_start");
-    s      = read_uint32("Block0_start");
-    p      = read_uint32("Block0_start");
-    offset = read_uint32("Block0_start");
-    o[0]   = read_uint32("Block0_start");
-    o[1]   = read_uint32("Block0_start");
-    o[2]   = read_uint32("Block0_start");
+    i[0]   = read_uint16("Block0_start");
+    i[1]   = read_uint16("Block0_start");
+    i[2]   = read_uint16("Block0_start");
+    k[0]   = read_uint16("Block0_start");
+    k[1]   = read_uint16("Block0_start");
+    k[2]   = read_uint16("Block0_start");
+    k[3]   = read_uint16("Block0_start");
+    s      = read_uint16("Block0_start");
+    p      = read_uint16("Block0_start");
+    offset = read_uint16("Block0_start");
+    offset = (offset << 16) + read_uint16("Block0_start");
+    o[0]   = read_uint16("Block0_start");
+    o[1]   = read_uint16("Block0_start");
+    o[2]   = read_uint16("Block0_start");
     __aa_barrier__();
 
     __ConvTransposeOptimized__(0,(i[0]>>2),0,i[1], 
                                i,k,o,offset,input,kernel,s,p,output);
     __aa_barrier__();
-    write_uint32 ("Block0_done", 1);
+    write_uint16 ("Block0_done", 1);
 }
 
 void convTransposeB()
 {
     uint16_t i[3],k[4],o[3],s,p;
     uint32_t offset;
-    i[0] = read_uint32("Block1_start");
-    i[1] = read_uint32("Block1_start");
-    i[2] = read_uint32("Block1_start");
-    k[0] = read_uint32("Block1_start");
-    k[1] = read_uint32("Block1_start");
-    k[2] = read_uint32("Block1_start");
-    k[3] = read_uint32("Block1_start");
-    s    = read_uint32("Block1_start");
-    p    = read_uint32("Block1_start");
-    offset = read_uint32("Block1_start");
-    o[0] = read_uint32("Block1_start");
-    o[1] = read_uint32("Block1_start");
-    o[2] = read_uint32("Block1_start");
+    i[0] = read_uint16("Block1_start");
+    i[1] = read_uint16("Block1_start");
+    i[2] = read_uint16("Block1_start");
+    k[0] = read_uint16("Block1_start");
+    k[1] = read_uint16("Block1_start");
+    k[2] = read_uint16("Block1_start");
+    k[3] = read_uint16("Block1_start");
+    s    = read_uint16("Block1_start");
+    p    = read_uint16("Block1_start");
+    offset = read_uint16("Block1_start");
+    offset = (offset << 16) + read_uint16("Block1_start");
+    o[0] = read_uint16("Block1_start");
+    o[1] = read_uint16("Block1_start");
+    o[2] = read_uint16("Block1_start");
     __aa_barrier__();
 
     __ConvTransposeOptimized__((i[0]>>2),(i[0]>>1),0,i[1],
                                i,k,o,offset,input,kernel,s,p,output);
     __aa_barrier__();
-    write_uint32 ("Block1_done", 1);
+    write_uint16 ("Block1_done", 1);
 }
 
 void convTransposeC()
 {
     uint16_t i[3],k[4],o[3],s,p;uint32_t offset;
-    i[0] = read_uint32("Block2_start");
-    i[1] = read_uint32("Block2_start");
-    i[2] = read_uint32("Block2_start");
-    k[0] = read_uint32("Block2_start");
-    k[1] = read_uint32("Block2_start");
-    k[2] = read_uint32("Block2_start");
-    k[3] = read_uint32("Block2_start");
-    s    = read_uint32("Block2_start");
-    p    = read_uint32("Block2_start");
-    offset = read_uint32("Block2_start");
-    o[0] = read_uint32("Block2_start");
-    o[1] = read_uint32("Block2_start");
-    o[2] = read_uint32("Block2_start");
+    i[0] = read_uint16("Block2_start");
+    i[1] = read_uint16("Block2_start");
+    i[2] = read_uint16("Block2_start");
+    k[0] = read_uint16("Block2_start");
+    k[1] = read_uint16("Block2_start");
+    k[2] = read_uint16("Block2_start");
+    k[3] = read_uint16("Block2_start");
+    s    = read_uint16("Block2_start");
+    p    = read_uint16("Block2_start");
+    offset = read_uint16("Block2_start");
+    offset = (offset << 16) + read_uint16("Block2_start");
+    o[0] = read_uint16("Block2_start");
+    o[1] = read_uint16("Block2_start");
+    o[2] = read_uint16("Block2_start");
     __aa_barrier__();
     __ConvTransposeOptimized__((i[0]>>1),((i[0]>>2) + (i[0]>>1)),0,i[1],
                                i,k,o,offset,input,kernel,s,p,output);
     __aa_barrier__();
-    write_uint32 ("Block2_done", 1);
+    write_uint16 ("Block2_done", 1);
 }
 
 void convTransposeD()
 {
     uint16_t i[3],k[4],o[3],s,p;uint32_t offset;
-    i[0] = read_uint32("Block3_start");
-    i[1] = read_uint32("Block3_start");
-    i[2] = read_uint32("Block3_start");
-    k[0] = read_uint32("Block3_start");
-    k[1] = read_uint32("Block3_start");
-    k[2] = read_uint32("Block3_start");
-    k[3] = read_uint32("Block3_start");
-    s    = read_uint32("Block3_start");
-    p    = read_uint32("Block3_start");
-    offset = read_uint32("Block3_start");
-    o[0] = read_uint32("Block3_start");
-    o[1] = read_uint32("Block3_start");
-    o[2] = read_uint32("Block3_start");
+    i[0] = read_uint16("Block3_start");
+    i[1] = read_uint16("Block3_start");
+    i[2] = read_uint16("Block3_start");
+    k[0] = read_uint16("Block3_start");
+    k[1] = read_uint16("Block3_start");
+    k[2] = read_uint16("Block3_start");
+    k[3] = read_uint16("Block3_start");
+    s    = read_uint16("Block3_start");
+    p    = read_uint16("Block3_start");
+    offset = read_uint16("Block3_start");
+    offset = (offset << 16) + read_uint16("Block3_start");
+    o[0] = read_uint16("Block3_start");
+    o[1] = read_uint16("Block3_start");
+    o[2] = read_uint16("Block3_start");
     __aa_barrier__();
     __ConvTransposeOptimized__(((i[0]>>2) + (i[0]>>1)),i[0],0,i[1],
                                i,k,o,offset,input,kernel,s,p,output);
     __aa_barrier__();    
-    write_uint32 ("Block3_done", 1);
+    write_uint16 ("Block3_done", 1);
 }
 
 void convTranspose()
@@ -216,67 +220,71 @@ void convTranspose()
 #ifndef SW
     uint64_t start_time = timer();
 #endif
-    write_uint32("Block0_start", inp_dim0);
-    write_uint32("Block0_start", inp_dim1);
-    write_uint32("Block0_start", inp_dim2);
-    write_uint32("Block0_start", ker_dim0);
-    write_uint32("Block0_start", ker_dim1);
-    write_uint32("Block0_start", ker_dim2);
-    write_uint32("Block0_start", ker_dim3);
-    write_uint32("Block0_start", stride0);
-    write_uint32("Block0_start", padding);
-    write_uint32("Block0_start", offset0);
-    write_uint32("Block0_start", out_dim0);
-    write_uint32("Block0_start", out_dim1);
-    write_uint32("Block0_start", out_dim2);
+    write_uint16("Block0_start", inp_dim0);
+    write_uint16("Block0_start", inp_dim1);
+    write_uint16("Block0_start", inp_dim2);
+    write_uint16("Block0_start", ker_dim0);
+    write_uint16("Block0_start", ker_dim1);
+    write_uint16("Block0_start", ker_dim2);
+    write_uint16("Block0_start", ker_dim3);
+    write_uint16("Block0_start", stride0);
+    write_uint16("Block0_start", padding);
+    write_uint16("Block0_start", offset0);
+    write_uint16("Block0_start", (offset0 >> 16));
+    write_uint16("Block0_start", out_dim0);
+    write_uint16("Block0_start", out_dim1);
+    write_uint16("Block0_start", out_dim2);
     
-    write_uint32("Block1_start", inp_dim0);
-    write_uint32("Block1_start", inp_dim1);
-    write_uint32("Block1_start", inp_dim2);
-    write_uint32("Block1_start", ker_dim0);
-    write_uint32("Block1_start", ker_dim1);
-    write_uint32("Block1_start", ker_dim2);
-    write_uint32("Block1_start", ker_dim3);
-    write_uint32("Block1_start", stride0);
-    write_uint32("Block1_start", padding);
-    write_uint32("Block1_start", offset1);
-    write_uint32("Block1_start", out_dim0);
-    write_uint32("Block1_start", out_dim1);
-    write_uint32("Block1_start", out_dim2);
+    write_uint16("Block1_start", inp_dim0);
+    write_uint16("Block1_start", inp_dim1);
+    write_uint16("Block1_start", inp_dim2);
+    write_uint16("Block1_start", ker_dim0);
+    write_uint16("Block1_start", ker_dim1);
+    write_uint16("Block1_start", ker_dim2);
+    write_uint16("Block1_start", ker_dim3);
+    write_uint16("Block1_start", stride0);
+    write_uint16("Block1_start", padding);
+    write_uint16("Block1_start", offset1);
+    write_uint16("Block1_start", (offset1 >> 16));
+    write_uint16("Block1_start", out_dim0);
+    write_uint16("Block1_start", out_dim1);
+    write_uint16("Block1_start", out_dim2);
      
-    write_uint32("Block2_start", inp_dim0);
-    write_uint32("Block2_start", inp_dim1);
-    write_uint32("Block2_start", inp_dim2);
-    write_uint32("Block2_start", ker_dim0);
-    write_uint32("Block2_start", ker_dim1);
-    write_uint32("Block2_start", ker_dim2);
-    write_uint32("Block2_start", ker_dim3);
-    write_uint32("Block2_start", stride0);
-    write_uint32("Block2_start", padding);
-    write_uint32("Block2_start", offset2); 
-    write_uint32("Block2_start", out_dim0);
-    write_uint32("Block2_start", out_dim1);
-    write_uint32("Block2_start", out_dim2);   
+    write_uint16("Block2_start", inp_dim0);
+    write_uint16("Block2_start", inp_dim1);
+    write_uint16("Block2_start", inp_dim2);
+    write_uint16("Block2_start", ker_dim0);
+    write_uint16("Block2_start", ker_dim1);
+    write_uint16("Block2_start", ker_dim2);
+    write_uint16("Block2_start", ker_dim3);
+    write_uint16("Block2_start", stride0);
+    write_uint16("Block2_start", padding);
+    write_uint16("Block2_start", offset2); 
+    write_uint16("Block2_start", (offset2 >> 16));
+    write_uint16("Block2_start", out_dim0);
+    write_uint16("Block2_start", out_dim1);
+    write_uint16("Block2_start", out_dim2);   
  
-    write_uint32("Block3_start", inp_dim0);
-    write_uint32("Block3_start", inp_dim1);
-    write_uint32("Block3_start", inp_dim2);
-    write_uint32("Block3_start", ker_dim0);
-    write_uint32("Block3_start", ker_dim1);
-    write_uint32("Block3_start", ker_dim2);
-    write_uint32("Block3_start", ker_dim3);
-    write_uint32("Block3_start", stride0);
-    write_uint32("Block3_start", padding);
-    write_uint32("Block3_start", offset3);
-    write_uint32("Block3_start", out_dim0);
-    write_uint32("Block3_start", out_dim1);
-    write_uint32("Block3_start", out_dim2);
+    write_uint16("Block3_start", inp_dim0);
+    write_uint16("Block3_start", inp_dim1);
+    write_uint16("Block3_start", inp_dim2);
+    write_uint16("Block3_start", ker_dim0);
+    write_uint16("Block3_start", ker_dim1);
+    write_uint16("Block3_start", ker_dim2);
+    write_uint16("Block3_start", ker_dim3);
+    write_uint16("Block3_start", stride0);
+    write_uint16("Block3_start", padding);
+    write_uint16("Block3_start", offset3);
+    write_uint16("Block3_start", (offset3 >> 16));
+    write_uint16("Block3_start", out_dim0);
+    write_uint16("Block3_start", out_dim1);
+    write_uint16("Block3_start", out_dim2);
     
 
-    uint16_t s0 = read_uint32("Block0_done");
-    uint16_t s1 = read_uint32("Block1_done");
-    uint16_t s2 = read_uint32("Block2_done");
-    uint16_t s3 = read_uint32("Block3_done");   
+    uint16_t s0 = read_uint16("Block0_done");
+    uint16_t s1 = read_uint16("Block1_done");
+    uint16_t s2 = read_uint16("Block2_done");
+    uint16_t s3 = read_uint16("Block3_done");   
     __aa_barrier__();
 #ifndef SW
     uint64_t stop_time = timer();

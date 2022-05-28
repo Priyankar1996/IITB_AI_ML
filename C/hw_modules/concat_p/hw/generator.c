516fc172c317b1439015228b598b08369025a0be
Priyankar Sarkar <priyansrk@gmail.com>
2022-05-28 15:27:11 +0530

    Added concat modules

---
+++ C/hw_modules/concat_p/hw/generator.c
@@ -0,0 +1,75 @@
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdint.h>
+#include "sized_tensor.h"
+
+#define __UpdateOutputDescriptorConcatTensors__(src1,src2,output) ({\
+	fprintf(stderr,"Updating output descriptor and writing inputs.\n");\
+    desc_output.dimensions[0] = src1.dimensions[0];\
+    desc_output.dimensions[1] = src1.dimensions[1] + src2.dimensions[1];\
+	desc_output.dimensions[2] = src1.dimensions[2];\
+})
+
+SizedTensor_16K input1,input2,output;
+TensorDescriptor desc_input1,desc_input2,desc_output;
+
+int main(int argc,char **argv)
+{
+    srand(time(0));
+    FILE *input_file,*out_file;
+    if ((input_file = fopen(argv[1],"r")) == NULL){
+        fprintf(stderr,"Input File Error\n");
+        exit(-1);
+    }
+    if ((out_file = fopen("CoutFile.txt","w")) == NULL){
+		fprintf(stderr,"Output File error\n");
+		exit(-1);
+	}
+    uint8_t rand_input_data;
+	fscanf(input_file,"%hhu",&rand_input_data);
+	desc_input1.data_type = u8;
+    desc_input2.data_type = u8;
+    desc_output.data_type = u8;
+
+    int ii;
+	for (ii = 0;ii < 3;ii++){
+		fscanf(input_file,"%hu",&desc_input1.dimensions[ii]);
+	    fprintf(out_file,"%u\n",(desc_input1.dimensions[ii]>>8));
+	    fprintf(out_file,"%u\n",(desc_input1.dimensions[ii]));        
+	}
+	fprintf(stderr,"Read input-1's descriptor %d,%d,%d.\n",desc_input1.dimensions[0],desc_input1.dimensions[1],desc_input1.dimensions[2]);
+    for (ii = 0;ii < 3;ii++){
+		fscanf(input_file,"%hu",&desc_input2.dimensions[ii]);
+	    fprintf(out_file,"%u\n",(desc_input2.dimensions[ii]>>8));
+	    fprintf(out_file,"%u\n",desc_input2.dimensions[ii]);
+	}
+	fprintf(stderr,"Read input-2's descriptor %d,%d,%d.\n",desc_input2.dimensions[0],desc_input2.dimensions[1],desc_input2.dimensions[2]);
+	uint32_t input1_size = desc_input1.dimensions[0]*desc_input1.dimensions[1]*desc_input1.dimensions[2];
+	uint32_t input2_size = desc_input2.dimensions[0]*desc_input2.dimensions[1]*desc_input2.dimensions[2];
+    __UpdateOutputDescriptorConcatTensors__(desc_input1,desc_input2,desc_output);
+    for(ii = 0;ii < 3;ii++){
+		fprintf(out_file,"%u\n",(desc_output.dimensions[ii]>>8));
+		fprintf(out_file,"%u\n",desc_output.dimensions[ii]);
+	}
+    uint8_t temp[8];
+    for (ii = 0; ii < input1_size; ii++)
+	{
+		if (rand_input_data)	temp[ii&3] = rand();	//Random data
+		else temp[ii&7] = (ii+1)%256;	
+		fprintf(out_file,"%u\n",temp[ii&7]);							//Sequential dat
+	}
+    for (ii = 0; ii < input2_size; ii++)
+	{
+		if (rand_input_data)	temp[ii&3] = rand();	//Random data
+		else temp[ii&7] = (ii+1)%256;	
+		fprintf(out_file,"%u\n",temp[ii&7]);							//Sequential dat
+	}
+	fprintf(stderr,"Wrote all input values\n");
+    fclose(input_file);
+	fclose(out_file);
+    return 0;
+}
---
+++ C/hw_modules/concat_p/hw/src/prog.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdint.h>
+#include <Pipes.h>
+#include "pipeHandler.h"
+#include "sized_tensor.h"
+#include "concat.h"
+
+SizedTensor_16K input_1,input_2,output;
+
+#ifndef SW
+void __loop_pipelining_on__(uint32_t pipeline_depth, uint32_t buffering, uint32_t full_rate);
+	#define __loop_pipeline_var__ __loop_pipelining_on__(15,1,1);
+void __aa_barrier__();
+#else
+	#define __loop_pipeline_var__ {;}
+	#define __aa_barrier__() {;}
+#endif
+
+#define __get8xi8__(element) ({\
+	element = read_uint8 ("Concat_input_pipe");\
+	element = (element << 8) + read_uint8 ("Concat_input_pipe");\
+	element = (element << 8) + read_uint8 ("Concat_input_pipe");\
+	element = (element << 8) + read_uint8 ("Concat_input_pipe");\
+    element = (element << 8) + read_uint8 ("Concat_input_pipe");\
+	element = (element << 8) + read_uint8 ("Concat_input_pipe");\
+	element = (element << 8) + read_uint8 ("Concat_input_pipe");\
+	element = (element << 8) + read_uint8 ("Concat_input_pipe");\
+})
+
+#define __set8xi8__(addr) ({\
+	uint64_t element = output.data_array[addr];\
+	uint8_t out_data[8];\
+	out_data[7] = element & 0xFF;\
+	element>>=8;\
+	out_data[6] = element & 0xFF;\
+	element>>=8;\
+	out_data[5]= element & 0xFF;\
+	element>>=8;\
+    out_data[4] = element & 0xFF;\
+	element>>=8;\
+	out_data[3] = element & 0xFF;\
+	element>>=8;\
+	out_data[2] = element & 0xFF;\
+	element>>=8;\
+    out_data[1] = element & 0xFF;\
+	element>>=8;\
+	out_data[0] = element & 0xFFFF;\
+	write_uint8 ("Concat_output_pipe",out_data[0]);\
+	write_uint8 ("Concat_output_pipe",out_data[1]);\
+	write_uint8 ("Concat_output_pipe",out_data[2]);\
+	write_uint8 ("Concat_output_pipe",out_data[3]);\
+    write_uint8 ("Concat_output_pipe",out_data[4]);\
+	write_uint8 ("Concat_output_pipe",out_data[5]);\
+	write_uint8 ("Concat_output_pipe",out_data[6]);\
+	write_uint8 ("Concat_output_pipe",out_data[7]);\
+})
+
+void concat()
+{
+    uint16_t input1_dim0,input1_dim1,input1_dim2,input2_dim0,input2_dim1,input2_dim2,out_dim0,out_dim1,out_dim2;
+    input1_dim0 = read_uint8 ("Concat_input_pipe");
+    input1_dim0 = (input1_dim0 << 8) + read_uint8 ("Concat_input_pipe");
+    input1_dim1 = read_uint8 ("Concat_input_pipe");
+    input1_dim1 = (input1_dim1 << 8) + read_uint8 ("Concat_input_pipe");
+    input1_dim2 = read_uint8 ("Concat_input_pipe");
+    input1_dim2 = (input1_dim2 << 8) + read_uint8 ("Concat_input_pipe");
+    input2_dim0 = read_uint8 ("Concat_input_pipe");
+    input2_dim0 = (input2_dim0 << 8) + read_uint8 ("Concat_input_pipe");
+    input2_dim1 = read_uint8 ("Concat_input_pipe");
+    input2_dim1 = (input2_dim1 << 8) + read_uint8 ("Concat_input_pipe");
+    input2_dim2 = read_uint8 ("Concat_input_pipe");
+    input2_dim2 = (input2_dim2 << 8) + read_uint8 ("Concat_input_pipe");
+    out_dim0 = read_uint8 ("Concat_input_pipe");
+    out_dim0 = (out_dim0 << 8) + read_uint8 ("Concat_input_pipe");
+    out_dim1 = read_uint8 ("Concat_input_pipe");
+    out_dim1 = (out_dim1 << 8) + read_uint8 ("Concat_input_pipe");
+    out_dim2 = read_uint8 ("Concat_input_pipe");
+    out_dim2 = (out_dim2 << 8) + read_uint8 ("Concat_input_pipe");
+
+    int i;
+    uint32_t input1_size = input1_dim0 * input1_dim1 * input1_dim2;
+    uint32_t input2_size = input2_dim0 * input2_dim1 * input2_dim2;
+    uint32_t output_size = out_dim0 * out_dim1 *out_dim2;
+    uint16_t count1 = (input1_dim1 * input1_dim2)>>3;
+    uint16_t count2 = (input2_dim1 * input2_dim2)>>3; 
+    for(i = 0; i < (input1_size >> 3); i ++)
+    {
+        uint64_t element;
+        __get8xi8__(element);
+        input_1.data_array[i] = element;
+    }
+
+    for(i = 0; i < (input2_size >> 3); i ++)
+    {
+        uint64_t element;
+        __get8xi8__(element);
+        input_2.data_array[i] = element;
+    }
+    __aa_barrier__();
+#ifndef SW
+    uint64_t start_time = timer();
+    __aa_barrier__();
+#endif
+    __ConcatTensors__(input_1,input_2,output,count1,count2,(output_size >> 3));
+    __aa_barrier__();
+#ifndef SW
+    uint64_t stop_time = timer();
+    uint64_t elapsed_time = stop_time - start_time;
+	uint8_t time_data[8];
+	time_data[7] = elapsed_time & 0xFF;
+	elapsed_time>>=8;
+	time_data[6] = elapsed_time & 0xFF;
+	elapsed_time>>=8;
+	time_data[5] = elapsed_time & 0xFF;
+	elapsed_time>>=8;
+    time_data[4] = elapsed_time & 0xFF;
+	elapsed_time>>=8;
+	time_data[3] = elapsed_time & 0xFF;
+	elapsed_time>>=8;
+	time_data[2] = elapsed_time & 0xFF;
+	elapsed_time>>=8;
+    time_data[1] = elapsed_time & 0xFF;
+	elapsed_time>>=8;
+	time_data[0] = elapsed_time & 0xFF;
+	write_uint8 ("Concat_output_pipe",time_data[0]);
+	write_uint8 ("Concat_output_pipe",time_data[1]);
+	write_uint8 ("Concat_output_pipe",time_data[2]);
+	write_uint8 ("Concat_output_pipe",time_data[3]);
+    write_uint8 ("Concat_output_pipe",time_data[4]);
+	write_uint8 ("Concat_output_pipe",time_data[5]);
+	write_uint8 ("Concat_output_pipe",time_data[6]);
+	write_uint8 ("Concat_output_pipe",time_data[7]);
+#endif    
+    __aa_barrier__();
+    for (i = 0; i < (output_size >> 3); i++){
+        __set8xi8__(i);
+    }
+}

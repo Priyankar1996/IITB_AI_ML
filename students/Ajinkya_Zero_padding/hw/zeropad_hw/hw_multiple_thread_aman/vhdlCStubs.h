d0623657bbe2b951a2f03baf0affeec36f66797d
deval1812 <dbpatel96@gmail.com>
2022-06-08 00:45:07 +0530

    Updated input file

---
+++ students/Ajinkya_Zero_padding/hw/zeropad_hw/hw_multiple_thread_aman/vhdlCStubs.h
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <Pipes.h>
+#include <SocketLib.h>
+void global_storage_initializer_();
+void progx_xoptx_xo_storage_initializer_();
+void sendOutput(uint32_t size);
+uint64_t timer();
+void timerDaemon();
+void zeropad3D();
+void zeropad3D_A();
+void zeropad3D_B();
+void zeropad3D_C();
+void zeropad3D_D();

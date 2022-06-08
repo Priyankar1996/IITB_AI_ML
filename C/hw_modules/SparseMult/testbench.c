024c45ea575a9b5bfb6c4ac5497fef2f61fcc618
= <=>
2022-06-08 12:02:55 +0530

    Added files for conv, and an implementation for sparse multiplier

---
+++ C/hw_modules/SparseMult/testbench.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <time.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#include "vhdlCStubs.h"
+
+
+int main(int argc, char**argv){
+
+	fprintf(stderr,"Entering testbench main program\n");
+	fprintf(stderr,"Start time %ld\n", read_uint64("output_pipe"));
+	int i_max = 10000;
+	
+	for (int i = 1; i <= i_max; i++){
+		if (i < i_max)
+			write_uint64("input_pipe",7445858815525322822);
+//			write_uint64("input_pipe",9064528483713091);
+//			write_uint64("input_pipe",9007353879068739);
+		else
+			write_uint64("input_pipe",257);
+		fprintf(stderr,"SENT element %d\n",i);
+	}
+
+	fprintf(stderr,"Time taken is for information load is %ld\n",read_uint64("output_pipe"));
+	fprintf(stderr,"End time for computation is %ld\n",read_uint64("output_pipe"));
+
+return 0;
+
+}

9d00e19d398254e8c80850685a84dea9540f996c
AjinkyaRaghuwanshi <ajinkyaraghuwanshi@gmail.com>
2022-06-12 13:28:05 +0530

    Making the aa version of zeropad

---
+++ students/Ajinkya_Zero_padding/hw/zeropad_hw/aa_hw_single_thread/src/zero_pad_opt.h
@@ -0,0 +1,64 @@
+//Assumptions : 3D tensor, row_major, int16 data type
+#ifndef __zero_pad_opt_h__
+#define __zero_pad_opt_h__
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <float.h>
+#include "sized_tensor.h"
+#include <inttypes.h>
+#include <assert.h>
+
+void zeropad3D();
+
+#ifndef SW
+void __loop_pipelining_on__(uint32_t pipeline_depth, uint32_t buffering, uint32_t full_rate);
+	#define __loop_pipeline_var__ __loop_pipelining_on__(15,1,1);
+#else
+	#define __loop_pipeline_var__ {;}
+#endif
+
+#define __dt__ int16_t
+#define __dim0__(td) ({td.dimensions[0];})
+#define __dim1__(td) ({td.dimensions[1];})
+#define __dim2__(td) ({td.dimensions[2];})
+
+#define __zero_pad_opt__(row_low,row_high,col_low,col_high,z_low,z_high,tx,ty,tz,rx,ry,rz,T,pad,R) ({\
+	uint16_t k = 0,j1 = col_low,i = row_low,pad_reg = pad,dim2T = tz,\
+			dim1T = ty,dim0T = tx,dim2R = rz,\
+			dim1R = ry,dim0R = rx,dim21T = dim2T*dim1T,\
+			dim21R = dim2R*dim1R,j = j1,break_flag;\
+	while (1) {\
+		__loop_pipeline_var__\
+		break_flag = __Check_break_flag__(i,j,k,row_high,col_high,dim2T,j1,pad_reg);\
+		__dt__ dest_data_array_idx = (k + dim2R*(j) + dim21R*(i)),img_data_array_idx = (k + dim2T*(j-pad_reg) + dim21T*(i-pad_reg));\
+		if((i < (pad_reg)) || (i >= (row_high+pad_reg)) || (j < (pad_reg)) || (j >= (col_high+pad_reg))){\
+            R.data_array[dest_data_array_idx >> 3] = 0 ;\
+            }\
+        else{\
+			R.data_array[dest_data_array_idx >> 3] = T.data_array[img_data_array_idx >> 3];\
+			}\
+		if(break_flag)\
+			break;\
+	}\
+})
+
+#define __Check_break_flag__(i,j,k,row_high,col_high,dim2T,j1,pad_reg) ({\
+	__dt__ flag = 0,dim2T_dif = dim2T - 8;\
+	if(k <= dim2T_dif)\
+		k+=8;\
+	else {\
+		k = 0;\
+		j++;\
+		if (j == (col_high + (pad_reg<<1)))\
+		{\
+			j = j1;\
+			i++;\
+		}\
+		if(i == (row_high + (pad_reg<<1)))\
+			flag = 1;\
+	}\
+	flag;\
+})
+#endif
--- students/Ajinkya_Zero_padding/hw/zeropad_hw/aa_hw_single_thread/testbench.c
+++ students/Ajinkya_Zero_padding/hw/zeropad_hw/aa_hw_single_thread/testbench.c
@@ -33,6 +33,14 @@
 
 #ifdef SW
 DEFINE_THREAD(zeropad3D);
+DEFINE_THREAD(zeropad3D_A);
+// DEFINE_THREAD(zeropad3D_B);
+// DEFINE_THREAD(zeropad3D_C);
+// DEFINE_THREAD(zeropad3D_D);
+// DEFINE_THREAD(zeropad3D_E);
+// DEFINE_THREAD(zeropad3D_F);
+// DEFINE_THREAD(zeropad3D_G);
+// DEFINE_THREAD(zeropad3D_H);
 #endif
 
 SizedTensor_16K T,R;
@@ -42,7 +50,7 @@ TensorDescriptor des_inp,des_out;
 int main(int argc,char **argv)
 {
     fprintf(stderr,"Entering testbench main program\n");
-	srand(time(0));
+    srand(time(0));
 
     FILE *input_file,*param_file, *out_file;
 
@@ -55,30 +63,62 @@ int main(int argc,char **argv)
         fprintf(stderr,"Input parameter file error\n");
         exit(-1);
     }
-    
+
     if ((out_file = fopen("CoutFile.txt","w")) == NULL){
 		fprintf(stderr,"Output File error\n");
 		exit(-1);
 	}
-    
     fprintf(stderr,"Defined and opened files\n");
 
     #ifdef SW
         init_pipe_handler();
         register_pipe ("zeropad_input_pipe",2,8,PIPE_FIFO_MODE);
         register_pipe ("zeropad_output_pipe",2,8,PIPE_FIFO_MODE);
+		register_pipe ("Block0_starting",1,8,PIPE_FIFO_MODE);
+		register_pipe ("Block0_complete",1,8,PIPE_FIFO_MODE);
+		// register_pipe ("Block1_starting",1,8,PIPE_FIFO_MODE);
+		// register_pipe ("Block1_complete",1,8,PIPE_FIFO_MODE);
+		// register_pipe ("Block2_starting",1,8,PIPE_FIFO_MODE);
+		// register_pipe ("Block2_complete",1,8,PIPE_FIFO_MODE);
+		// register_pipe ("Block3_starting",1,8,PIPE_FIFO_MODE);
+		// register_pipe ("Block3_complete",1,8,PIPE_FIFO_MODE);
+		// register_pipe ("Block4_starting",1,8,PIPE_FIFO_MODE);
+		// register_pipe ("Block4_complete",1,8,PIPE_FIFO_MODE);
+		// register_pipe ("Block5_starting",1,8,PIPE_FIFO_MODE);
+		// register_pipe ("Block5_complete",1,8,PIPE_FIFO_MODE);
+		// register_pipe ("Block6_starting",1,8,PIPE_FIFO_MODE);
+		// register_pipe ("Block6_complete",1,8,PIPE_FIFO_MODE);
+		// register_pipe ("Block7_starting",1,8,PIPE_FIFO_MODE);
+		// register_pipe ("Block7_complete",1,8,PIPE_FIFO_MODE);
 
-        
+
 		PTHREAD_DECL(zeropad3D);
-		
+		PTHREAD_DECL(zeropad3D_A);
+		// PTHREAD_DECL(zeropad3D_B);
+		// PTHREAD_DECL(zeropad3D_C);
+		// PTHREAD_DECL(zeropad3D_D);
+		// PTHREAD_DECL(zeropad3D_E);
+		// PTHREAD_DECL(zeropad3D_F);
+		// PTHREAD_DECL(zeropad3D_G);
+		// PTHREAD_DECL(zeropad3D_H);
+
+
 		PTHREAD_CREATE(zeropad3D);
+		PTHREAD_CREATE(zeropad3D_A);
+		// PTHREAD_CREATE(zeropad3D_B);
+		// PTHREAD_CREATE(zeropad3D_C);
+		// PTHREAD_CREATE(zeropad3D_D);
+		// PTHREAD_CREATE(zeropad3D_E);
+		// PTHREAD_CREATE(zeropad3D_F);
+		// PTHREAD_CREATE(zeropad3D_G);
+		// PTHREAD_CREATE(zeropad3D_H);
 
     #endif
 
     fprintf(stderr,"Reading files\n");
-    uint16_t rand_input_data;
+    uint16_t rand_input_data, rand_kernel_data;
 	fscanf(input_file,"%hhd",&rand_input_data);
-    
+
     //Take datatype as input
     #ifdef __U8
 		des_inp.data_type = u8;
@@ -92,18 +132,17 @@ int main(int argc,char **argv)
 	uint8_t c1,c2;
     fscanf(input_file,"%u",&c1);
 	fscanf(input_file,"%u",&c2);
-	// write_uint8("zeropad_input_pipe",c1);
-	// write_uint8("zeropad_input_pipe",c2);
+	write_uint8("zeropad_input_pipe",c1);
+	write_uint8("zeropad_input_pipe",c2);
 	des_inp.row_major_form = (c1 << 8) + c2;
 	fprintf(stderr,"Read the row major form\n");
 
 	fscanf(input_file,"%u",&c1);
 	fscanf(input_file,"%u",&c2);
-	// write_uint8("zeropad_input_pipe",c1);
-	// write_uint8("zeropad_input_pipe",c2);
+	write_uint8("zeropad_input_pipe",c1);
+	write_uint8("zeropad_input_pipe",c2);
 	des_inp.number_of_dimensions = (c1 << 8) + c2;
 	fprintf(stderr,"Read the number of dimensions\n");
-
 
 	int ii;
 	for (ii = 0;ii < des_inp.number_of_dimensions;ii++){
@@ -119,20 +158,17 @@ int main(int argc,char **argv)
 
 	fscanf(param_file,"%d",&pad);
 	write_uint8("zeropad_input_pipe",pad);
-	
+
 	fprintf(stderr,"Read pad value:%d\n",pad);
+
 	__UpdateOutputDescriptorZeropadTensors__(des_inp,pad,des_out);
-    fprintf(stderr,"Read output descriptor %d,%d,%d.\n",des_out.dimensions[0],des_out.dimensions[1],des_out.dimensions[2]);
-	
+	fprintf(stderr,"Read output descriptor %d,%d,%d.\n",des_out.dimensions[0],des_out.dimensions[1],des_out.dimensions[2]);
+
 	for(ii = 0;ii < des_inp.number_of_dimensions;ii++){
 		write_uint8("zeropad_input_pipe",des_out.dimensions[ii]>>8);
 		write_uint8("zeropad_input_pipe",des_out.dimensions[ii]);
 	}
-
-	// uint64_t input_size = __NumberOfElementsInSizedTensor__(T);
 	uint64_t input_size = des_inp.dimensions[0]*des_inp.dimensions[1]*des_inp.dimensions[2];
-	fprintf(stderr,"Input size:%d\n",input_size);
-	fprintf(stderr,"Now starting to read input data\n");
 
     if (des_inp.data_type == u8){
 		uint8_t temp[8];
@@ -151,13 +187,14 @@ int main(int argc,char **argv)
 		fprintf(stderr,"Error. Datatypes mismatch.\n");
 	}
 	fprintf(stderr,"Wrote all input values\n");
-	
+
     fprintf(stderr,"Reading the output values from hardware\n");
 	fprintf(out_file,"\n");
 	int size = des_out.dimensions[0]*des_out.dimensions[1]*des_out.dimensions[2];
 	fprintf(stderr,"Size of output is %d,%d,%d\n",des_out.dimensions[0],des_out.dimensions[1],des_out.dimensions[2]);
-	uint64_t time_taken;
-	#ifndef SW
+
+#ifndef SW
+		uint64_t time_taken;
 		time_taken = read_uint8 ("zeropad_output_pipe");
 		time_taken = (time_taken << 8) + read_uint8 ("zeropad_output_pipe");
 		time_taken = (time_taken << 8) + read_uint8 ("zeropad_output_pipe");
@@ -166,7 +203,8 @@ int main(int argc,char **argv)
 		time_taken = (time_taken << 8) + read_uint8 ("zeropad_output_pipe");
 		time_taken = (time_taken << 8) + read_uint8 ("zeropad_output_pipe");
 		time_taken = (time_taken << 8) + read_uint8 ("zeropad_output_pipe");
-	#endif
+		fprintf(stderr,"Time taken is %lu\n",time_taken);
+#endif
 
 	if (des_out.data_type == u8){
 		uint8_t val;
@@ -181,21 +219,14 @@ int main(int argc,char **argv)
 		fprintf(stderr,"Error. Datypes mismatch.\n");
 	}
     fprintf(stderr,"Read back the values from hardware\n");
-	fprintf(stderr,"Time taken is %lu\n",time_taken);
 
 	fclose(input_file);
 	fclose(param_file);
 	fclose(out_file);
 
-	
-	// #ifndef SW
-	// 	uint64_t time_taken = read_uint64("elapsed_time_pipe");
-	// 	fprintf(stderr,"Time taken is %lu\n",time_taken);
-	// #endif
-
     #ifdef SW
 	    PTHREAD_CANCEL(zeropad3D);
-		// PTHREAD_CANCEL(zeropad3D_A);
+		PTHREAD_CANCEL(zeropad3D_A);
 		// PTHREAD_CANCEL(zeropad3D_B);
 		// PTHREAD_CANCEL(zeropad3D_C);
 		// PTHREAD_CANCEL(zeropad3D_D);
@@ -203,7 +234,6 @@ int main(int argc,char **argv)
 		// PTHREAD_CANCEL(zeropad3D_F);
 		// PTHREAD_CANCEL(zeropad3D_G);
 		// PTHREAD_CANCEL(zeropad3D_H);
-
 	    close_pipe_handler();
     #endif
 return 0;

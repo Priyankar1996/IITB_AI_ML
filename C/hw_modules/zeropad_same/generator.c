0fdbd94350475ba463308aeba028ec5738693171
Priyankar Sarkar <priyansrk@gmail.com>
2022-06-13 13:26:26 +0530

    Implemented same padding.

---
+++ C/hw_modules/zeropad_same/generator.c
@@ -0,0 +1,184 @@
+#define __U8 1
+
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdint.h>
+#include "sized_tensor.h"
+
+
+#define __UpdateOutputDescriptorConvTransTensors__(src,kernel,stride,padding,output) ({\
+	fprintf(stderr,"Updating output descriptor and writing inputs.\n");\
+	int ji;\
+    desc_output.dimensions[0] = (src.dimensions[0]-1)*stride + desc_kernel.dimensions[1] + 1 - (2*padding);\
+    desc_output.dimensions[1] = (src.dimensions[1]-1)*stride + desc_kernel.dimensions[2] + 1 - (2*padding);\
+	desc_output.dimensions[2] = src.dimensions[2];\
+})
+
+#define __dt__ int16_t
+
+
+TensorDescriptor desc_input,desc_kernel,desc_output;
+
+int main(int argc,char **argv)
+{
+    fprintf(stderr,"Entering testbench main program\n");
+    srand(time(0));
+
+    FILE *input_file,*param_file, *kernel_file, *out_file;
+
+    if ((input_file = fopen(argv[1],"r")) == NULL){
+        fprintf(stderr,"Input File Error\n");
+        exit(-1);
+    }
+
+    if ((kernel_file = fopen(argv[2],"r")) ==  NULL){
+        fprintf(stderr,"Input kernel file error\n");
+        exit(-1);
+    }
+
+    if ((param_file = fopen(argv[3],"r")) == NULL){
+        fprintf(stderr,"Parameters File Error\n");
+        exit(-1);
+    }
+    
+    if ((out_file = fopen("OutFile.txt","w")) == NULL){
+		fprintf(stderr,"Output File error\n");
+		exit(-1);
+	}
+    
+    fprintf(stderr,"Defined and opened files\n");
+
+    fprintf(stderr,"Reading files\n");
+    uint16_t rand_input_data, rand_kernel_data;
+	fscanf(input_file,"%hhd",&rand_input_data);
+	fscanf(kernel_file,"%hhd",&rand_kernel_data);
+    
+    //Take datatype as input
+    	#ifdef __U8
+		desc_input.data_type = u8;
+        desc_kernel.data_type = u8;
+		desc_output.data_type = u8;
+	#endif
+	#ifdef __U16
+		desc_input.data_type = u16;
+        desc_kernel.data_type = u16;
+	#endif
+	#ifdef __U32
+		desc_input.data_type = u32;
+        desc_kernel.data_type = u32;
+	#endif
+	#ifdef __U64
+		desc_input.data_type = u64;
+        desc_kernel.data_type = u64;
+	#endif
+	#ifdef __I8
+		desc_input.data_type = i8;
+        desc_kernel.data_type = i8;
+	#endif
+	#ifdef __I16
+		desc_input.data_type = i16;
+		desc_kernel.data_type = i16;
+		desc_output.data_type = i16;
+	#endif
+	#ifdef __I32
+		desc_input.data_type = i32;
+        desc_kernel.data_type = i32;
+	#endif
+	#ifdef __I64
+		desc_input.data_type = i64;
+        desc_kernel.data_type = i64;
+	#endif
+	#ifdef __F8
+		desc_input.data_type = float8;
+        desc_kernel.data_type = float8;
+	#endif
+	#ifdef __F16
+        desc_input.data_type = float16;
+        desc_kernel.data_type = float16;
+	#endif
+	#ifdef __F32
+		desc_input.data_type = float32;
+        desc_kernel.data_type = float32;
+	#endif
+	#ifdef __F64
+		desc_input.data_type = float64;
+        desc_kernel.data_type = float64;
+	#endif
+
+	int ii;
+	for (ii = 0;ii < 3;ii++){
+		uint8_t var1,var2;
+		fscanf(input_file,"%u",&var1);
+		fscanf(input_file,"%u",&var2);
+		desc_input.dimensions[ii] = (var1 << 8) + var2;
+	    fprintf(out_file,"%hu\n",(desc_input.dimensions[ii] >> 8));        
+	    fprintf(out_file,"%hu\n",(desc_input.dimensions[ii]));
+	}
+	fprintf(stderr,"Read input descriptor %d,%d,%d.\n",desc_input.dimensions[0],desc_input.dimensions[1],desc_input.dimensions[2]);
+
+
+	for (ii = 0;ii < 4;ii++){
+		uint8_t var1,var2;
+		fscanf(kernel_file,"%u",&var1);
+		fscanf(kernel_file,"%u",&var2);
+		desc_kernel.dimensions[ii] = (var1 << 8) + var2;
+	    fprintf(out_file,"%hu\n",(desc_kernel.dimensions[ii] >> 8));        
+	    fprintf(out_file,"%hu\n",(desc_kernel.dimensions[ii]));
+	}
+	fprintf(stderr,"Read kernel descriptor %d,%d,%d,%d.\n",desc_kernel.dimensions[0],desc_kernel.dimensions[1],desc_kernel.dimensions[2],desc_kernel.dimensions[3]);
+
+    uint16_t stride[2];
+
+    for(ii = 0;ii < 2;ii++){
+		uint8_t var1,var2;
+		fscanf(param_file,"%u",&var1);
+		fscanf(param_file,"%u",&var2);
+		stride[ii] = (var1 << 8) + var2;
+	    fprintf(out_file,"%hu\n",(stride[ii] >> 8));        
+	    fprintf(out_file,"%hu\n",(stride[ii]));
+	}
+
+	fprintf(stderr,"Read stride values: %d.\n",stride[0]);
+	fprintf(stderr,"Read pad value:%d\n",stride[1]);
+    
+	__UpdateOutputDescriptorConvTransTensors__(desc_input,desc_kernel,stride[0],stride[1],desc_output);
+	for(ii = 0;ii < 3;ii++){
+		fprintf(out_file,"%hd\n",(desc_output.dimensions[ii]>>8));
+		fprintf(out_file,"%hd\n",desc_output.dimensions[ii]);
+	}
+	uint64_t input_size = desc_input.dimensions[0]*desc_input.dimensions[1]*desc_input.dimensions[2];
+    uint64_t kernel_size = desc_kernel.dimensions[0]*desc_kernel.dimensions[1]*desc_kernel.dimensions[2]*desc_kernel.dimensions[3];
+
+    if (desc_input.data_type == u8){
+		uint8_t temp[8];
+		for (ii = 0; ii < input_size; ii++)
+		{
+			if (rand_input_data)	temp[ii&7] = rand();	//Random data
+			else temp[ii&7] = (ii+1)%255;	
+			fprintf(out_file,"%hd\n",temp[ii&7]);							//Sequential data
+		}
+		
+		for (ii = 0; ii < kernel_size; ii++)
+		{
+			if (rand_kernel_data)	temp[ii&7] = rand();	//Random data
+			else temp[ii&7] = (ii+1)%255;					//Sequential data
+			fprintf(out_file,"%hd\n",temp[ii&3]);
+		}
+	}	
+	else{
+		fprintf(stderr,"Error. Datatypes mismatch.\n");
+	}
+	fprintf(stderr,"Wrote all input values\n");
+
+	fclose(input_file);
+	fclose(param_file);
+    fclose(kernel_file);
+	fclose(out_file);
+
+   
+return 0;
+}

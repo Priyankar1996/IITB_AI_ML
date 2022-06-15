c10fd59d0c09c4888f663edd8fd1173b0bd17617
deval1812 <dbpatel96@gmail.com>
2022-06-15 15:35:37 +0530

    Update to convolution

--- C/hw_modules/convolution/hw/testbench.c
+++ C/hw_modules/convolution/hw/testbench.c
@@ -23,7 +23,7 @@
 //
 SizedTensor_16K T,K,R;
 TensorDescriptor desc_T,desc_K,desc_R;
-uint8_t stride;
+uint8_t stride = 1;
 
 #ifdef SW
 DEFINE_THREAD(conv2D);
@@ -80,19 +80,14 @@ int main(int argc,char **argv)
     fprintf(stderr,"Entering testbench main program\n");
 	srand(time(0));
 
-    FILE *input_file,*kernel_file,*stride_file,*out_file;
+    FILE *input_file,*kernel_file,*out_file;
 
     if ((input_file = fopen(argv[1],"r")) == NULL){
         fprintf(stderr,"Input File Error\n");
         exit(-1);
     }
 
-    if ((stride_file = fopen(argv[2],"r")) ==  NULL){
-        fprintf(stderr,"Stride file error\n");
-        exit(-1);
-    }
-
-	if ((kernel_file = fopen(argv[3],"r")) ==  NULL){
+	if ((kernel_file = fopen(argv[2],"r")) ==  NULL){
         fprintf(stderr,"Kernel file error\n");
         exit(-1);
     }
@@ -139,32 +134,56 @@ int main(int argc,char **argv)
 	desc_R.data_type = i16;
 
 	fprintf(stderr,"Reading files\n");
-    	uint16_t rand_input_data;
-	fscanf(input_file,"%hhd",&rand_input_data);
-	uint16_t rand_kernel_data;
-	fscanf(kernel_file,"%hhd",&rand_kernel_data);
+    	uint8_t c1,c2;
+    	fscanf(input_file,"%u",&c1);
+	fscanf(input_file,"%u",&c2);
+	write_uint8("conv_input_pipe",c1);
+	write_uint8("conv_input_pipe",c2);
+	desc_T.row_major_form = (c1 << 8) + c2;
+	fprintf(stderr,"Read the row major form\n");
 
-	fscanf(input_file,"%hhd",&desc_T.row_major_form);
-	write_uint8("conv_input_pipe",desc_T.row_major_form);
-    	fscanf(input_file,"%d",&desc_T.number_of_dimensions);
-	write_uint8("conv_input_pipe",desc_T.number_of_dimensions);
+	fscanf(input_file,"%u",&c1);
+	fscanf(input_file,"%u",&c2);
+	write_uint8("conv_input_pipe",c1);
+	write_uint8("conv_input_pipe",c2);
+	desc_T.number_of_dimensions = (c1 << 8) + c2;
+	fprintf(stderr,"Read the number of dimensions\n");
 	int ii;
 	for (ii = 0;ii < desc_T.number_of_dimensions;ii++){
-		fscanf(input_file,"%d",&desc_T.dimensions[ii]);
-        	write_uint8("conv_input_pipe",desc_T.dimensions[ii]);
+		uint8_t var1,var2;
+		fscanf(input_file,"%u",&var1);
+		fscanf(input_file,"%u",&var2);
+        	write_uint8("conv_input_pipe",var1);
+		write_uint8("conv_input_pipe",var2);
+		desc_T.dimensions[ii] = (var1 << 8) + var2;
+		// des_inp.dimensions[ii] = var1;
 	}
 	fprintf(stderr,"Read input descriptor %d,%d,%d.\n",desc_T.dimensions[0],desc_T.dimensions[1],desc_T.dimensions[2]);
 
-	fscanf(stride_file,"%d",&stride);
 	write_uint8("conv_input_pipe",stride);
 
-	fscanf(kernel_file,"%hhd",&desc_K.row_major_form);
-	write_uint8("conv_input_pipe",desc_K.row_major_form);
-    	fscanf(kernel_file,"%d",&desc_K.number_of_dimensions);
-	write_uint8("conv_input_pipe",desc_K.number_of_dimensions);
+	fscanf(kernel_file,"%u",&c1);
+	fscanf(kernel_file,"%u",&c2);
+	write_uint8("conv_input_pipe",c1);
+	write_uint8("conv_input_pipe",c2);
+	desc_K.row_major_form = (c1 << 8) + c2;
+	fprintf(stderr,"Read the row major form %d\n",desc_K.row_major_form);
+
+	fscanf(kernel_file,"%u",&c1);
+	fscanf(kernel_file,"%u",&c2);
+	write_uint8("conv_input_pipe",c1);
+	write_uint8("conv_input_pipe",c2);
+	desc_K.number_of_dimensions = (c1 << 8) + c2;
+	fprintf(stderr,"Read the number of dimensions %d\n",desc_K.number_of_dimensions);
 	for (ii = 0;ii < desc_K.number_of_dimensions;ii++){
-		fscanf(kernel_file,"%d",&desc_K.dimensions[ii]);
-        	write_uint8("conv_input_pipe",desc_K.dimensions[ii]);
+		uint8_t var1,var2;
+		fscanf(kernel_file,"%u",&var1);
+		fscanf(kernel_file,"%u",&var2);
+        	write_uint8("conv_input_pipe",var1);
+		write_uint8("conv_input_pipe",var2);
+		desc_K.dimensions[ii] = (var1 << 8) + var2;
+		fprintf(stderr,"%d\n",desc_K.dimensions[ii]);
+		// des_inp.dimensions[ii] = var1;
 	}
 	fprintf(stderr,"Read kernel descriptor %d,%d,%d,%d.\n",desc_K.dimensions[0],desc_K.dimensions[1],desc_K.dimensions[2],desc_K.dimensions[3]);
 
@@ -175,8 +194,7 @@ int main(int argc,char **argv)
 		int64_t temp;
 		for (ii = 0; ii < (T_input_size >> 2)+1; ii++)
 		{
-			if (rand_input_data)	temp = rand();	//Random data
-			else temp = 0x0005000500050005;
+			temp = 0x0005000500050005;
 			write_uint8("conv_input_pipe",(temp>>56));	
 			write_uint8("conv_input_pipe",(temp>>48));
 			write_uint8("conv_input_pipe",(temp>>40));
@@ -189,8 +207,7 @@ int main(int argc,char **argv)
 		
 		for (ii = 0; ii < (K_input_size >> 2)+1; ii++)
 		{
-			if (rand_kernel_data)	temp = rand();	//Random data
-			else temp = 0x0001000100010001;					//Sequential data
+			temp = 0x0001000100010001;					//Sequential data
 			write_uint8("conv_input_pipe",(temp>>56));	
 			write_uint8("conv_input_pipe",(temp>>48));
 			write_uint8("conv_input_pipe",(temp>>40));
@@ -229,7 +246,6 @@ int main(int argc,char **argv)
 
 	fclose(input_file);
 	fclose(kernel_file);
-	fclose(stride_file);
 	fclose(out_file);
 
 	#ifndef SW

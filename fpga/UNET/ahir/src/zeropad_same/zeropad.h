0da1c40f3da5f3dc51cb12db4e26e67fccaff592
Priyankar Sarkar <priyansrk@gmail.com>
2022-06-18 12:28:42 +0530

    Added return type of modules

--- fpga/UNET/ahir/src/zeropad_same/zeropad.h
+++ fpga/UNET/ahir/src/zeropad_same/zeropad.h
@@ -20,5 +20,5 @@ uint8_t zeropad_same(uint16_t,uint16_t,uint16_t,uint16_t,uint16_t,uint16_t,uint8
 void zeropad(uint16_t input_dim0,uint16_t input_dim1,uint16_t input_dim2,uint16_t out_dim0,uint16_t out_dim1,uint16_t out_dim2,uint8_t index1, uint8_t index2)
 {
     uint8_t done_flag = zeropad_same(input_dim0,input_dim1,input_dim2,out_dim0,out_dim1,out_dim2,index1,index2);
-	write_uint8("system_out_pipe",done_flag);
+	write_uint8("system_output_pipe",done_flag);
 } 

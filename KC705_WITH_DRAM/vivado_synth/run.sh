eaf7dc8cca6cb7b17f12cfddb63de30c04af9690
= <amandhammani2@gmail.com>
2022-08-01 14:35:08 +0530

    Added ai engine to KC705_WITH_DRAM

--- KC705_WITH_DRAM/vivado_synth/run.sh
+++ KC705_WITH_DRAM/vivado_synth/run.sh
@@ -1,6 +1,4 @@
-cd /home/kamran/my_xil_dir/Vivado/2017.1
-source settings64.sh
-cd /home/kamran/Desktop/all_new/kc705/vivado_synth
+source /home/AIML/my_xil_dir/Vivado/2017.1/settings64.sh
 vivado -mode batch -source my_bitgen.tcl
 
 
